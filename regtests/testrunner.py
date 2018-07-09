37a41a3b3da3f8db52f457466535e9faba918c26
Lauri Himanen <lauri.himanen@aalto.fi>
2018-07-09 18:15:08 +0300

    Added sine matrix tests to the testsuite.

--- regtests/testrunner.py
+++ regtests/testrunner.py
@@ -6,6 +6,7 @@ import sys
 # Import the test modules
 import coulombmatrix
 import ewaldmatrix
+import sinematrix
 import acsf
 
 
@@ -16,6 +17,7 @@ suite = unittest.TestSuite()
 # Add tests to the test suite
 suite.addTests(loader.loadTestsFromModule(coulombmatrix))
 suite.addTests(loader.loadTestsFromModule(ewaldmatrix))
+suite.addTests(loader.loadTestsFromModule(sinematrix))
 suite.addTests(loader.loadTestsFromModule(acsf))
 
 
