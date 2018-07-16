Fixed issue in LMBTR coming from new normalization parameter.

describe/descriptors/lmbtr.py
@@ -103,14 +103,15 @@ class LMBTR(MBTR):
             ValueError if the given k value is not supported, or the weighting
             is not specified for periodic systems.
         """
-        atomic_numbers.append(0) #Ghost
+        atomic_numbers.append(0)  # Ghost
         super().__init__(
                     atomic_numbers,
                     k,
                     periodic,
                     grid,
                     weighting,
-                    flatten,
+                    normalize=False,
+                    flatten=flatten,
                     )
 
     def update(self):
@@ -124,7 +125,7 @@ class LMBTR(MBTR):
             print("Warning: K = 1 is deprecated for LMBTR")
 
     def describe(self,
-                 system, 
+                 system,
                  list_atom_indices=None,
                  list_positions=None,
                  scaled_positions=False
@@ -134,16 +135,16 @@ class LMBTR(MBTR):
 
         Args:
             system (System): The system for which the descriptor is created.
-            list_atom_indices (iterable): indices of atoms, from which 
+            list_atom_indices (iterable): indices of atoms, from which
                                           local_mbtr is needed
-            list_positions (iterable): positions of points, from which 
-                                       local_mbtr is needed 
+            list_positions (iterable): positions of points, from which
+                                       local_mbtr is needed
             scaled_positions (boolean): if list of positions are scaled
                                         use only if system allows it
 
         Returns:
             1D ndarray: The local many-body tensor representations of given postions,
-                        for k terms, as an array. These are ordered as given in 
+                        for k terms, as an array. These are ordered as given in
                         list_atom_indices, followed by list_positions
         """
         system_new = system.copy()
@@ -153,27 +154,30 @@ class LMBTR(MBTR):
             list_atom_indices.sort()
             for atom_index in list_atom_indices:
                 if atom_index >= len(system):
-                    raise ValueError("Atom index: {}, larger than total number of atoms ".format(atom_index))
+                    raise ValueError("Atom index: {}, larger than total number of atoms.".format(atom_index))
             list_atoms += list_atom_indices
         elif list_positions is not None:
-            if scaled_positions: #convert positions to cartesian
+            if scaled_positions:  # convert positions to cartesian
                 if np.linalg.norm(system.get_cell()) == 0:
-                    raise RuntimeError("System doesn't have cell, to justify scaled positions")
+                    raise ValueError("System doesn't have cell to justify scaled positions.")
                 list_positions = np.dot(system.get_cell(), np.array(list_positions).T).T.tolist()
-            
+
             # Checking if given position exists
             for i, pos in enumerate(list_positions):
                 if np.sum( np.linalg.norm(system.get_positions() - pos, axis=1) == 0):
-                    raise RuntimeError(("position {}: {} exists in system,"
-                                        + " give its index in list_atom_indices").format(i, pos))
+                    raise ValueError(
+                        "Position {}: {} exists in system, give its index in "
+                        "list_atom_indices."
+                        .format(i, pos)
+                    )
             old_len = len(system_new)
-            system_new += System('X{}'.format(len(list_positions)), 
-                                  positions=list_positions
-                                 )
+            system_new += System('X{}'.format(len(list_positions)),
+                                positions=list_positions
+                                )
             list_atoms += list(range(old_len, len(system_new)))
         else:
-            raise RuntimeError("create method requires list_atom_indices and/or list_positions")
-              
+            raise ValueError("create method requires list_atom_indices and/or list_positions")
+
         desc = np.empty(len(list_atoms), dtype='object')
 
         for i, self.atom_index in enumerate(list_atoms):
@@ -189,7 +193,7 @@ class LMBTR(MBTR):
             int: Number of features for this descriptor.
         """
         n_features = 0
-        n_elem = self.n_elements - 1 #Removing ghost
+        n_elem = self.n_elements - 1  # Removing ghost
 
         if 1 in self.k:
             n_k1_grid = self.get_k1_settings()["n"]
@@ -361,7 +365,7 @@ class LMBTR(MBTR):
         self._axis_k2 = np.linspace(start, stop, n)
 
         inv_dist_dict = self.inverse_distances(system)
-        n_elem = self.n_elements - 1 #removing ghost
+        n_elem = self.n_elements - 1  # removing ghost
 
         if self.flatten:
             k2 = lil_matrix(
@@ -375,7 +379,7 @@ class LMBTR(MBTR):
             weighting_function = self.weighting["k2"]["function"]
 
         m = -1
-        for i in range(1, n_elem+1): #ignoring Ghost
+        for i in range(1, n_elem+1):  # ignoring Ghost
                 m += 1
                 try:
                     inv_dist = np.array(inv_dist_dict[i])
@@ -418,7 +422,7 @@ class LMBTR(MBTR):
 
         cos_dict, cos_weight_dict = self.cosines_and_weights(system)
 
-        n_elem = self.n_elements - 1 #removing ghost
+        n_elem = self.n_elements - 1  # removing ghost
 
         if self.flatten:
             k3 = lil_matrix(
@@ -430,8 +434,8 @@ class LMBTR(MBTR):
         # k >= i. E.g. angles OHH are the same as HHO. This will half the size
         # of the K3 input.
         m = -1
-        for i in range(1, n_elem+1): #ignoring ghost
-            for j in range(1, n_elem+1): #ignoring ghost
+        for i in range(1, n_elem+1):  # ignoring ghost
+            for j in range(1, n_elem+1):  # ignoring ghost
                 try:
                     cos_values = np.array(cos_dict[i][j])
                 except KeyError:

regtests/lmbtr.py
@@ -6,7 +6,6 @@ import numpy as np
 import unittest
 
 from describe.descriptors import LMBTR
-from describe.data.element_data import numbers_to_symbols
 
 from ase import Atoms
 
@@ -77,58 +76,59 @@ class LMBTRTests(unittest.TestCase):
                 k={1, 4},
                 periodic=False,
             )
-    
+
     def test_create(self):
-        '''
+        """
         LMBTR: Test all fail-safe in create method
-        '''
+        """
         decay_factor = 0.5
         lmbtr = LMBTR(
-        atomic_numbers=[1, 8],
-        k=[1, 2],
-        periodic=True,
-        grid={
-            "k1": {
-                "min": 10,
-                "max": 18,
-                "sigma": 0.1,
-                "n": 200,
+            atomic_numbers=[1, 8],
+            k=[1, 2],
+            periodic=True,
+            grid={
+                "k1": {
+                    "min": 10,
+                    "max": 18,
+                    "sigma": 0.1,
+                    "n": 200,
+                },
+                "k2": {
+                    "min": 0,
+                    "max": 0.7,
+                    "sigma": 0.01,
+                    "n": 200,
+                },
+                "k3": {
+                    "min": -1.0,
+                    "max": 1.0,
+                    "sigma": 0.05,
+                    "n": 200,
+                }
             },
-            "k2": {
-                "min": 0,
-                "max": 0.7,
-                "sigma": 0.01,
-                "n": 200,
+            weighting={
+                "k2": {
+                    "function": lambda x: np.exp(-decay_factor*x),
+                    "threshold": 1e-3
+                },
+                "k3": {
+                    "function": lambda x: np.exp(-decay_factor*x),
+                    "threshold": 1e-3
+                },
             },
-            "k3": {
-                "min": -1.0,
-                "max": 1.0,
-                "sigma": 0.05,
-                "n": 200,
-            }
-        },
-        weighting={
-            "k2": {
-                "function": lambda x: np.exp(-decay_factor*x),
-                "threshold": 1e-3
-            },
-            "k3": {
-                "function": lambda x: np.exp(-decay_factor*x),
-                "threshold": 1e-3
-            },
-        },
-        flatten=False)
-        
+            flatten=False
+        )
+
         with self.assertRaises(ValueError):
             desc = lmbtr.create(H2O, list_atom_indices=[3])
 
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(ValueError):
             desc = lmbtr.create(H2O)
 
-        with self.assertRaises(RuntimeError):
-            desc = lmbtr.create(H2O, list_positions=[[0,0,0]])
+        with self.assertRaises(ValueError):
+            desc = lmbtr.create(H2O, list_positions=[[0, 0, 0]])
 
-        with self.assertRaises(RuntimeError):
+        with self.assertRaises(ValueError):
             H = Atoms(
                 positions=[[0, 0, 0]],
                 symbols=["H"],
@@ -136,10 +136,9 @@ class LMBTRTests(unittest.TestCase):
 
             desc = lmbtr.create(
                 H,
-                list_positions=[[0,0,1]],
+                list_positions=[[0, 0, 1]],
                 scaled_positions=True
             )
-
 
     def test_number_of_features(self):
         """LMBTR: Tests that the reported number of features is correct.
@@ -236,9 +235,9 @@ class LMBTRTests(unittest.TestCase):
         # self.assertEqual(cm.shape, (25,))
 
     def test_periodic(self):
-        '''
+        """
         LMBTR: Test periodic flag
-        '''
+        """
         test_sys = Atoms(
             cell=[[5.0, 0.0, 0.0], [0, 5.0, 0.0], [0.0, 0.0, 5.0]],
             positions=[[0, 0, 1], [0, 0, 3]],
@@ -271,16 +270,16 @@ class LMBTRTests(unittest.TestCase):
             },
             flatten=False
         )
-            
+
         desc = lmbtr.create(test_sys, list_atom_indices=[0])
         desc_ = lmbtr.create(test_sys_, list_atom_indices=[0])
 
         self.assertTrue(np.linalg.norm(desc_[0][0] - desc[0][0]) < 1e-6)
 
     def test_inverse_distances(self):
-        '''
+        """
         LMBTR: Test inverse distances
-        '''
+        """
         lmbtr = LMBTR([1, 8], k=[2], periodic=False)
         lmbtr.create(H2O, list_atom_indices=[1])
         inv_dist = lmbtr._inverse_distances
@@ -300,9 +299,9 @@ class LMBTRTests(unittest.TestCase):
         self.assertEqual(inv_dist, inv_dist_2)
 
     def test_cosines(self):
-        '''
+        """
         LMBTR: Test cosines
-        '''
+        """
         lmbtr = LMBTR([1, 8], k=[3], periodic=False)
         lmbtr.create(H2O, list_atom_indices=[1])
         angles = lmbtr._angles
@@ -324,141 +323,6 @@ class LMBTRTests(unittest.TestCase):
         lmbtr.create(H2O_2, list_atom_indices=[0])
         angles2 = lmbtr._angles
         self.assertEqual(angles, angles2)
-
-#    def test_gaussian_distribution(self):
-#        """LMBTR: Check that the broadening follows gaussian distribution.
-#        """
-#        std = 1
-#        start = -3
-#        stop = 11
-#        n = 500
-#        lmbtr = LMBTR(
-#            [1, 8],
-#            k=[1],
-#            grid={
-#                "k1": {
-#                    "min": start,
-#                    "max": stop,
-#                    "sigma": std,
-#                    "n": n
-#                }
-#            },
-#            periodic=False,
-#            flatten=False)
-#        y = lmbtr.create(H2O)
-#        k1_axis = lmbtr._axis_k1
-#
-#        # Find the location of the peaks
-#        peak1_x = np.searchsorted(k1_axis, 1)
-#        peak1_y = y[0][0, peak1_x]
-#        peak2_x = np.searchsorted(k1_axis, 8)
-#        peak2_y = y[0][1, peak2_x]
-#
-#        # Check against the analytical value
-#        gaussian = lambda x, mean, sigma: np.exp(-(x-mean)**2/(2*sigma**2))
-#        self.assertTrue(np.allclose(peak1_y, 2*gaussian(1, 1, std), rtol=0, atol=0.001))
-#        self.assertTrue(np.allclose(peak2_y, gaussian(8, 8, std), rtol=0, atol=0.001))
-#
-#        # Check the integral
-#        pdf = y[0][0, :]
-#        # mpl.plot(pdf)
-#        # mpl.show()
-#        dx = (stop-start)/(n-1)
-#        sum_cum = np.sum(0.5*dx*(pdf[:-1]+pdf[1:]))
-#        exp = 2/(1/math.sqrt(2*math.pi*std**2))
-#        self.assertTrue(np.allclose(sum_cum, exp, rtol=0, atol=0.001))
-
-    # def test_k1(self):
-        # lmbtr = LMBTR([1, 8], k=[1], periodic=False, flatten=False)
-        # desc = lmbtr.create(H2O)
-        # x1 = lmbtr._axis_k1
-
-        # imap = lmbtr.index_to_atomic_number
-        # smap = {}
-        # for index, number in imap.items():
-            # smap[index] = numbers_to_symbols(number)
-
-        # Visually check the contents
-        # mpl.plot(y)
-        # mpl.ylim(0, y.max())
-        # mpl.show()
-
-        # mpl.plot(x1, desc[0][0, :], label="{}".format(smap[0]))
-        # mpl.plot(x1, desc[0][1, :], linestyle=":", linewidth=3, label="{}".format(smap[1]))
-        # mpl.ylabel("$\phi$ (arbitrary units)", size=20)
-        # mpl.xlabel("Inverse distance (1/angstrom)", size=20)
-        # mpl.legend()
-        # mpl.show()
-
-    # def test_k2(self):
-        # lmbtr = LMBTR([1, 8], k=[2], periodic=False, flatten=False)
-        # desc = lmbtr.create(H2O)
-
-        # x2 = lmbtr._axis_k2
-        # imap = lmbtr.index_to_atomic_number
-        # smap = {}
-        # for index, number in imap.items():
-            # smap[index] = numbers_to_symbols(number)
-
-        # Visually check the contents
-        # mpl.plot(x2, desc[1][0, 1, :], label="{}-{}".format(smap[0], smap[1]))
-        # mpl.plot(x2, desc[1][1, 0, :], linestyle=":", linewidth=3, label="{}-{}".format(smap[1], smap[0]))
-        # mpl.plot(x2, desc[1][1, 1, :], label="{}-{}".format(smap[1], smap[1]))
-        # mpl.plot(x2, desc[1][0, 0, :], label="{}-{}".format(smap[0], smap[0]))
-        # mpl.ylabel("$\phi$ (arbitrary units)", size=20)
-        # mpl.xlabel("Inverse distance (1/angstrom)", size=20)
-        # mpl.legend()
-        # mpl.show()
-
-        # lmbtr = LMBTR([1, 8], k=2, periodic=False, flatten=True)
-        # desc = lmbtr.create(H2O)
-        # y = desc.todense().T
-        # mpl.plot(y)
-        # mpl.show()
-
-    # def test_k3(self):
-        # lmbtr = LMBTR([1, 8], k=3, periodic=False)
-        # desc = lmbtr.create(H2O)
-        # y = desc.todense().T
-
-        # # Visually check the contents
-        # mpl.plot(y)
-        # mpl.show()
-
-    # def test_distances_duplicate(self):
-        # lmbtr = LMBTR([1, 8], k=[2], periodic=False)
-        # lmbtr.create(H2O)
-
-        # # Check that there are correct number of inverse distances
-        # n_atoms = len(H2O)
-        # n_ext_atoms = (1+2*1)**3*n_atoms
-        # n_inv_dist_analytic = sum([n_ext_atoms-i for i in range(1, n_atoms+1)])
-        # inv_dist = lmbtr._inverse_distances
-
-        # n_inv_dist = 0
-        # for dict1 in inv_dist.values():
-            # for val in dict1.values():
-                # n_inv_dist += len(val)
-
-        # self.assertEqual(n_inv_dist_analytic, n_inv_dist)
-
-    # def test_angles_duplicate(self):
-        # lmbtr = LMBTR([1, 8], n_atoms_max=2, k=3, periodic=False)
-        # lmbtr.create(H2O)
-
-        # Check that there are correct number of angles
-        # n_atoms = len(H2O)
-        # n_ext_atoms = (1+2*n_copies)**3*n_atoms
-        # n_angles_analytic = ?  # Did not have the energy to figure out the correct analytic formula... :)
-        # angles = lmbtr._angles
-
-        # n_angles = 0
-        # for dict1 in angles.values():
-            # for dict2 in dict1.values():
-                # for val in dict2.values():
-                    # n_angles += len(val)
-
-        # self.assertEqual(n_angles_analytic, n_angles)
 
     def test_symmetries(self):
         """LMBTR: Tests translational and rotational symmetries for a finite system.
@@ -518,7 +382,7 @@ class LMBTRTests(unittest.TestCase):
             self.assertTrue(deviation < 1e-6)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     suites = []
     suites.append(unittest.TestLoader().loadTestsFromTestCase(LMBTRTests))
     alltests = unittest.TestSuite(suites)