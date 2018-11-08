8b028085b6461777994561986dd4f0951e80d270
Lauri Himanen <lauri.himanen@aalto.fi>
2018-11-08 16:53:59 +0200

    Fixed coverage test issue.

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -4,4 +4,4 @@ pip install .
 
 # Compile CMBTR extension with cython. The .so file is not generated, as it
 # will be compiled during package setup
-cythonize describe/libmbtr/cmbtrwrapper.pyx
+cythonize dscribe/libmbtr/cmbtrwrapper.pyx
--- regtests/runtests.sh
+++ regtests/runtests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd regtests
 export COVERAGE_FILE="../.coverage"
-coverage run --source="describe" testrunner.py
+coverage run --source="dscribe" testrunner.py
