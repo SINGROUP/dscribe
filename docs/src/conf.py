36902f0e399be0c99f861866ebadfcaf152e7dfc
Lauri Himanen <lauri.himanen@aalto.fi>
2018-11-14 12:04:18 +0200

    Updated the documentation page look with logo and some css.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -96,14 +96,14 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {
-    # 'logo_only': True,
-# }
+html_theme_options = {
+    'logo_only': True,
+}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = "./_static/img/logo.png"
+html_logo = "./_static/img/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
