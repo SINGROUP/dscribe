8e30a1201185606446cea0be9f851647e0c40294
Lauri Himanen <lauri.himanen@aalto.fi>
2018-11-28 18:23:32 +0200

    Added sphinx as a dvelopment dependency in hope of in future building doc automatically with Travis.

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -5,3 +5,5 @@ pip install .
 # Compile CMBTR extension with cython. The .so file is not generated, as it
 # will be compiled during package setup
 cythonize dscribe/libmbtr/cmbtrwrapper.pyx
+
+# TODO: Compile documentation with Sphinx
