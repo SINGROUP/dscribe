57b4601f5835fb5790563d2656d5a23b25c9e4ac
Lauri Himanen <lauri.himanen@aalto.fi>
2018-12-11 21:29:37 +0200

    Updated docs, fixed doc-building script.

--- doc.sh
+++ doc.sh
@@ -2,13 +2,13 @@
 cd docs/src
 sphinx-apidoc -o ./doc ../../dscribe
 make html
-cp build/html/ ../
+cp -a build/html/. ../
 rm -r build
-
 
 # Push changes to docs
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
+cd ../..
 git add ./docs
 git commit -m "Travis documentation build: $TRAVIS_BUILD_NUMBER"
 git push --quiet https://SINGROUP:$GH_TOKEN@github.com/SINGROUP/dscribe development &>/dev/null
