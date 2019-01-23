a49f85c0c9512e99d0461c90fdddaa35e046e624
Lauri Himanen <lauri.himanen@aalto.fi>
2019-01-23 21:26:20 +0200

    Added Travis CI command to skip testing the documentation build made by Travis itself.

--- doc.sh
+++ doc.sh
@@ -11,5 +11,5 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 cd ../..
 git add ./docs
-git commit -m "Travis documentation build: $TRAVIS_BUILD_NUMBER"
+git commit -m "[skip travis] Travis documentation build: $TRAVIS_BUILD_NUMBER"
 git push --quiet https://SINGROUP:$GH_TOKEN@github.com/SINGROUP/dscribe master &>/dev/null
