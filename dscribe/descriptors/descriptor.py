Fixed issues with wrong offset in parallel calculation.

dscribe/descriptors/descriptor.py
@@ -193,6 +193,7 @@ class Descriptor(with_metaclass(ABCMeta)):
         is_sparse = self._sparse
         k, m = divmod(n_samples, n_jobs)
         jobs = (inp[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n_jobs))
+        # print(list(jobs))
 
         # Calculate the result in parallel with joblib
         if output_sizes is None:
@@ -230,13 +231,14 @@ class Descriptor(with_metaclass(ABCMeta)):
                         results.append(i_out)
                     else:
                         results[offset:offset+i_out.shape[0], :] = i_out
-                        offset += i_out.shape[0]
 
                 if verbose:
                     current_percent = (i_sample+1)/n_samples*100
                     if current_percent >= old_percent + 1:
                         old_percent = current_percent
                         print("Process {0}: {1:.1f} %".format(index, current_percent))
+
+                offset += i_out.shape[0]
 
             if is_sparse:
                 data = np.concatenate(data)
@@ -247,12 +249,14 @@ class Descriptor(with_metaclass(ABCMeta)):
             return (results, index)
 
         # We use the loky-backend provided by joblib because it allows proper
-        # serialization of member functions.
+        # serialization of methods.
         if backend == "multiprocessing":
             joblib_backend = "loky"
         else:
             joblib_backend = backend
 
+        # print(list(jobs)[0])
+        # print(output_sizes)
         with parallel_backend(joblib_backend, n_jobs=n_jobs):
             vec_lists = Parallel()(delayed(create_multiple)(i_args, func, is_sparse, n_features, n_desc, index, verbose) for index, (i_args, n_desc) in enumerate(zip(jobs, output_sizes)))
 

dscribe/descriptors/matrixdescriptor.py
@@ -115,7 +115,7 @@ class MatrixDescriptor(Descriptor):
 
         # Flatten the matrix if requested
         if self._flatten:
-            matrix = matrix.flatten()
+            matrix = np.reshape(matrix, (1, np.product(matrix.shape)))
 
         # If a sparse matrix is requested, convert to coo_matrix
         if self._sparse:

regtests/ewaldmatrix.py
@@ -83,7 +83,7 @@ class EwaldMatrixTests(TestBaseClass, unittest.TestCase):
         # Flattened
         desc = EwaldMatrix(n_atoms_max=5, permutation="none", flatten=True)
         matrix = desc.create(H2O)
-        self.assertEqual(matrix.shape, (25,))
+        self.assertEqual(matrix.shape, (1, 25))
 
     def test_sparse(self):
         """Tests the sparse matrix creation.
@@ -105,7 +105,17 @@ class EwaldMatrixTests(TestBaseClass, unittest.TestCase):
         desc = EwaldMatrix(n_atoms_max=5, permutation="none", flatten=True, sparse=False)
         n_features = desc.get_number_of_features()
 
-        # Test multiple systems
+        # Test multiple systems, serial job
+        output = desc.create(
+            system=samples,
+            n_jobs=1,
+        )
+        assumed = np.empty((2, n_features))
+        assumed[0, :] = desc.create(samples[0])
+        assumed[1, :] = desc.create(samples[1])
+        self.assertTrue(np.allclose(output, assumed))
+
+        # Test multiple systems, parallel job
         output = desc.create(
             system=samples,
             n_jobs=2,
@@ -123,7 +133,17 @@ class EwaldMatrixTests(TestBaseClass, unittest.TestCase):
         desc = EwaldMatrix(n_atoms_max=5, permutation="none", flatten=True, sparse=True)
         n_features = desc.get_number_of_features()
 
-        # Test when position given as indices
+        # Test multiple systems, serial job
+        output = desc.create(
+            system=samples,
+            n_jobs=1,
+        ).toarray()
+        assumed = np.empty((2, n_features))
+        assumed[0, :] = desc.create(samples[0]).toarray()
+        assumed[1, :] = desc.create(samples[1]).toarray()
+        self.assertTrue(np.allclose(output, assumed))
+
+        # Test multiple systems, parallel job
         output = desc.create(
             system=samples,
             n_jobs=2,