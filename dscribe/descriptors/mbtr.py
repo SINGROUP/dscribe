Fixed issue MBTR related to non-equivalent spectras for periodic systems. This issue was caused ultimately by a wrong index used in extending the system and the wrongly handled weighting in the k3 term.

dscribe/descriptors/mbtr.py
@@ -461,8 +461,8 @@ class MBTR(Descriptor):
             # If needed, create the extended system
             system_k2 = system
             if self.periodic:
-                system_k2 = self.create_extended_system(system, 2)
-            self.k2_geoms_and_weights(system_k2)
+                system_k2, copy_indices = self.create_extended_system(system, 2)
+            self.k2_geoms_and_weights(system_k2, copy_indices)
 
             # Free memory
             system_k2 = None
@@ -471,8 +471,8 @@ class MBTR(Descriptor):
             # If needed, create the extended system
             system_k3 = system
             if self.periodic:
-                system_k3 = self.create_extended_system(system, 3)
-            self.k3_geoms_and_weights(system_k3)
+                system_k3, copy_indices = self.create_extended_system(system, 3)
+            self.k3_geoms_and_weights(system_k3, copy_indices)
 
             # Free memory
             system_k3 = None
@@ -540,8 +540,7 @@ class MBTR(Descriptor):
             most have a weight that is larger or equivalent to the given
             threshold.
         """
-
-        # We need to speciy that the relative positions should not be wrapped.
+        # We need to specify that the relative positions should not be wrapped.
         # Otherwise the repeated systems may overlap with the positions taken
         # with get_positions()
         relative_pos = np.array(primitive_system.get_scaled_positions(wrap=False))
@@ -567,7 +566,7 @@ class MBTR(Descriptor):
             n_copies = -1
             while (not limit_found):
                 n_copies += 1
-                distance = n_copies*cell_vector_lengths[0]
+                distance = n_copies*cell_vector_lengths[i_axis]
 
                 # For terms k>2 we double the distances to take into
                 # account the "loop" that is required.
@@ -576,7 +575,7 @@ class MBTR(Descriptor):
 
                 weight = function(distance)
                 if weight < cutoff:
-                    n_copies_axis[i_axis] = n_copies
+                    n_copies_axis[i_axis] = n_copies+1
                     limit_found = True
 
         # Create copies of the cell but keep track of the atoms in the
@@ -588,6 +587,11 @@ class MBTR(Descriptor):
         a = np.array([1, 0, 0])
         b = np.array([0, 1, 0])
         c = np.array([0, 0, 1])
+
+        copy_indices = []
+        for i_copy_atom in range(len(primitive_system)):
+            copy_indices.append((0, 0, 0))
+
         for i in range(-n_copies_axis[0], n_copies_axis[0]+1):
             for j in range(-n_copies_axis[1], n_copies_axis[1]+1):
                 for k in range(-n_copies_axis[2], n_copies_axis[2]+1):
@@ -638,6 +642,9 @@ class MBTR(Descriptor):
                     pos_extended.append(valid_pos)
                     num_extended.append(valid_num)
 
+                    for i_copy_atom in range(len(valid_num)):
+                        copy_indices.append((i, j, k))
+
         pos_extended = np.concatenate(pos_extended)
         num_extended = np.concatenate(num_extended)
 
@@ -648,7 +655,12 @@ class MBTR(Descriptor):
             pbc=False
         )
 
-        return extended_system
+        print("Indices, atoms: {}, {}".format(len(copy_indices), len(extended_system)))
+
+        # from ase.visualize import view
+        # print(len(extended_system))
+        # view(extended_system)
+        return extended_system, copy_indices
 
     def gaussian_sum(self, centers, weights, settings):
         """Calculates a discrete version of a sum of Gaussian distributions.
@@ -714,7 +726,7 @@ class MBTR(Descriptor):
             self._k1_geoms, self._k1_weights = cmbtr.get_k1_geoms_and_weights(geom_func=b"atomic_number", weight_func=b"unity", parameters=parameters)
         return self._k1_geoms, self._k1_weights
 
-    def k2_geoms_and_weights(self, system):
+    def k2_geoms_and_weights(self, system, copy_indices):
         """Calculates the value of the geometry function and corresponding
         weights for unique two-body combinations.
 
@@ -733,6 +745,7 @@ class MBTR(Descriptor):
                 system.get_atomic_numbers(),
                 self.atomic_number_to_index,
                 interaction_limit=self._interaction_limit,
+                indices=copy_indices,
                 is_local=self._is_local
             )
 
@@ -756,7 +769,7 @@ class MBTR(Descriptor):
             )
         return self._k2_geoms, self._k2_weights
 
-    def k3_geoms_and_weights(self, system):
+    def k3_geoms_and_weights(self, system, copy_indices):
         """Calculates the value of the geometry function and corresponding
         weights for unique three-body combinations.
 
@@ -776,6 +789,7 @@ class MBTR(Descriptor):
                 system.get_atomic_numbers(),
                 self.atomic_number_to_index,
                 interaction_limit=self._interaction_limit,
+                indices=copy_indices,
                 is_local=self._is_local
             )
 
@@ -856,6 +870,9 @@ class MBTR(Descriptor):
 
         k2_geoms, k2_weights = self._k2_geoms, self._k2_weights
         n_elem = self.n_elements
+
+        # print(np.unique(k2_geoms[(0, 0)]))
+        # print(np.unique(k2_weights[(0, 0)]))
 
         # Depending of flattening, use either a sparse matrix or a dense one.
         if self._flatten:

examples/mbtr.py
@@ -4,71 +4,178 @@ atomic_numbers = [1, 8]
 n = 100
 
 # Setting up the MBTR descriptor
-mbtr = MBTR(
-    species=atomic_numbers,
-    k=2,
-    periodic=False,
-    grid={
-        "k2": {"min": 0, "max": 1, "n": n, "sigma": 0.1}
-    },
-    weighting=None
-)
+# mbtr = MBTR(
+    # species=atomic_numbers,
+    # k=2,
+    # periodic=False,
+    # grid={
+        # "k2": {"min": 0, "max": 1, "n": n, "sigma": 0.1}
+    # },
+    # weighting=None
+# )
 
-# Creating an atomic system as an ase.Atoms-object
-from ase.build import molecule
-import ase.data
+# # Creating an atomic system as an ase.Atoms-object
+# from ase.build import molecule
+# import ase.data
 
-water = molecule("H2O")
+# water = molecule("H2O")
 
-# Create MBTR output for the system
-mbtr_water = mbtr.create(water)
+# # Create MBTR output for the system
+# mbtr_water = mbtr.create(water)
 
-print(mbtr_water)
-print(mbtr_water.shape)
+# # print(mbtr_water)
+# # print(mbtr_water.shape)
 
 from ase.build import bulk
-nacl = bulk("NaCl", "rocksalt", a=5.64)
+# nacl = bulk("NaCl", "rocksalt", a=5.64)
 
-# Optionally we can preserve the tensorial nature of the data by specifying
-# "Flatten" as False. In this case the output will be a list of
-# multidimensional numpy arrays for each k-term. This form is easier to
-# visualize as done in the following.
-import matplotlib.pyplot as plt
+# # Optionally we can preserve the tensorial nature of the data by specifying
+# # "Flatten" as False. In this case the output will be a list of
+# # multidimensional numpy arrays for each k-term. This form is easier to
+# # visualize as done in the following.
+import matplotlib.pyplot as mpl
 
-decay = 0.5
-mbtr = MBTR(
-    species=[11, 17],
-    k=[1, 2, 3],
-    periodic=True,
-    grid={
-        "k1": {"min": 10, "max": 18, "sigma": 0.1, "n": 200},
-        "k2": {"min": 0, "max": 0.7, "sigma": 0.01, "n": 200},
-        "k3": {"min": -1.0, "max": 1.0, "sigma": 0.05, "n": 200}
-    },
-    weighting={
-        "k2": {"function": "exponential", "scale": decay, "cutoff": 1e-3},
-        "k3": {"function": "exponential", "scale": decay, "cutoff": 1e-3},
-    },
-    flatten=False,
-    sparse=False
-)
-mbtr_output = mbtr.create(nacl)
+# decay = 0.5
+# mbtr = MBTR(
+    # species=[11, 17],
+    # k=[1, 2, 3],
+    # periodic=True,
+    # grid={
+        # "k1": {"min": 10, "max": 18, "sigma": 0.1, "n": 200},
+        # "k2": {"min": 0, "max": 0.7, "sigma": 0.01, "n": 200},
+        # "k3": {"min": -1.0, "max": 1.0, "sigma": 0.05, "n": 200}
+    # },
+    # weighting={
+        # "k2": {"function": "exponential", "scale": decay, "cutoff": 1e-3},
+        # "k3": {"function": "exponential", "scale": decay, "cutoff": 1e-3},
+    # },
+    # flatten=False,
+    # sparse=False
+# )
+# mbtr_output = mbtr.create(nacl)
 
 # Create the mapping between an index in the output and the corresponding
 # chemical symbol
-n_elements = len(set(nacl.get_atomic_numbers()))
-imap = mbtr.index_to_atomic_number
-smap = {}
-for index, number in imap.items():
-    smap[index] = ase.data.chemical_symbols[number]
+# n_elements = len(set(nacl.get_atomic_numbers()))
+# imap = mbtr.index_to_atomic_number
+# smap = {}
+# for index, number in imap.items():
+    # smap[index] = ase.data.chemical_symbols[number]
 
-# Plot K2
-x2 = mbtr._axis_k2
-for i in range(n_elements):
-    for j in range(n_elements):
-        if j >= i:
-            plt.plot(x2, mbtr_output["k2"][i, j, :], label="{}-{}".format(smap[i], smap[j]))
-plt.ylabel("$\phi$ (a.u.)", size=20)
-plt.xlabel("Inverse distance (1/angstrom)", size=20)
-plt.legend()
-plt.show()
+# # Plot K2
+# x2 = mbtr._axis_k2
+# for i in range(n_elements):
+    # for j in range(n_elements):
+        # if j >= i:
+            # plt.plot(x2, mbtr_output["k2"][i, j, :], label="{}-{}".format(smap[i], smap[j]))
+# plt.ylabel("$\phi$ (a.u.)", size=20)
+# plt.xlabel("Inverse distance (1/angstrom)", size=20)
+# plt.legend()
+# plt.show()
+
+# Periodic
+import numpy as np
+
+decay = 1
+desc = MBTR(
+    species=[1],
+    k=[3],
+    periodic=True,
+    grid={
+        "k1": {
+            "min": 0,
+            "max": 2,
+            "sigma": 0.1,
+            "n": 100,
+        },
+        "k2": {
+            "min": 0,
+            "max": 1.0,
+            "sigma": 0.02,
+            "n": 200,
+        },
+        "k3": {
+            "min": -1.0,
+            "max": 1.0,
+            "sigma": 0.02,
+            "n": 200,
+        },
+    },
+    weighting={
+        "k2": {
+            "function": "exponential",
+            "scale": decay,
+            "cutoff": 1e-3
+        },
+        "k3": {
+            "function": "exponential",
+            "scale": decay,
+            "cutoff": 1e-3
+        },
+    },
+    flatten=True,
+    sparse=False,
+)
+
+# Testing that the same crystal, but different unit cells will have a
+# similar spectrum after normalization.
+# a1 = bulk('H', 'fcc', a=2.0)
+from ase.visualize import view
+from ase import Atoms
+
+a = Atoms(
+    cell=[1, 1, 1],
+    scaled_positions=[[0.5, 0.5, 0.5]],
+    symbols=["H"],
+)
+# view(a)
+# a1 = a*[2, 1, 1]
+# a2 = a*[1, 2, 1]
+# a3 = a*[1, 1, 2]
+
+a1 = bulk('H', 'fcc', a=2.0)
+a2 = bulk('H', 'fcc', a=2.0, orthorhombic=True)
+a3 = bulk('H', 'fcc', a=2.0, cubic=True)
+
+# a2 = a1*[2,2,1]
+# a3 = a1*[3,3,1]
+# a4 = a1*[4,4,1]
+
+# view(a1)
+# view(a2)
+# view(a3)
+
+# o1 = desc.create(a1)
+# print(len(desc._k3_geoms[(0, 0, 0)]))
+# geoms1 = sorted(desc._k3_geoms.values())
+# o2 = desc.create(a2)
+# print(len(desc._k3_geoms[(0, 0, 0)]))
+# geoms2 = sorted(desc._k2_geoms.values())
+# o3 = desc.create(a3)
+# print(len(desc._k3_geoms[(0, 0, 0)]))
+# geoms3 = sorted(desc._k2_geoms.values())
+
+# print(np.allclose(geoms1, geoms2, atol=1e-2))
+# print(np.allclose(geoms1, geoms3))
+
+output = desc.create([a1, a2, a3])
+# output = desc.create([a1, a2, a3])
+# output = desc.create([a1])
+
+# Plot the MBTR output for the different cells before normalization
+n_feat = desc.get_number_of_features()
+# mpl.plot(desc._axis_k3, output[0, :], label="x")
+# mpl.plot(desc._axis_k3, output[1, :], label="y")
+# mpl.plot(desc._axis_k3, output[3, :], label="z")
+# mpl.legend()
+# mpl.show()
+
+# Normalize the output to unit euclidean length
+output /= np.linalg.norm(output, axis=1)[:, np.newaxis]
+
+# Plot the MBTR output for the different cells after normalization
+mpl.plot(range(n_feat), output[0, :], label="Triclinic cell")
+mpl.plot(range(n_feat), output[1, :], label="Orthorhombic cell")
+mpl.plot(range(n_feat), output[2, :], label="Cubic cell")
+# mpl.plot(range(n_feat), output[3, :], label="Cubic cell")
+mpl.show()