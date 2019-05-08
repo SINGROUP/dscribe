Added licence template to the library code, fixed issue with undefined MBTR normalization.

dscribe/descriptors/soap.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function
-from builtins import super
-import numpy as np
+"""Copyright 2019 DScribe developers
 
-from joblib import Parallel, delayed, parallel_backend
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from __future__ import absolute_import, division, print_function, unicode_literals
+from builtins import (bytes, str, open, super, range, zip, round, input, int, pow, object)
+import numpy as np
 
 from scipy.sparse import coo_matrix
 