ce0bd4327594baf5a03e8fe3e48e2d8f41e2b738
Lauri Himanen <lauri.himanen@aalto.fi>
2019-07-03 23:28:37 +0300

    Trying to get the numpy installation working correctly on Travis+Python3.4

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -1,6 +1,13 @@
 # Update pip
 pip install pip --update
 
+# Install numpy before requirements, as we need a specific version for
+# different python versions, and using a requirements file cannot ensure the
+# installation of a specific version
+if [$TRAVIS_PYTHON_VERSION <= "3.5"]
+then
+    pip install numpy==1.15.4
+
 # Development dependencies
 pip install -r devrequirements.txt
 
