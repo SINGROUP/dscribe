28d4424525df00ddb8fb569b2665868634771748
Lauri Himanen <lauri.himanen@aalto.fi>
2019-12-14 00:46:35 +0200

    Added the new ext folder.

---
+++ dscribe/ext/ext.cpp
@@ -0,0 +1,23 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>  // Enables easy access to numpy arrays
+#include <pybind11/stl.h>    // Enables automatic type conversion from C++ containers to python
+#include "celllist.h"
+#include "soapGTO.h"
+#include "soapGeneral.h"
+
+namespace py = pybind11;
+using namespace std;
+
+// Notice that the name of the first argument to the module macro needs to
+// correspond to the file name!
+PYBIND11_MODULE(ext, m) {
+    // SOAP
+    m.def("soap_gto", &soapGTO, "SOAP with gaussian type orbital radial basis set.");
+    m.def("soap_general", &soapGeneral, "SOAP with a general radial basis set.");
+
+    // CellList
+    py::class_<CellList>(m, "CellList")
+        .def(py::init<py::array_t<double>, double>())
+        .def("get_neighbours_for_index", &CellList::getNeighboursForIndex)
+        .def("get_neighbours_for_position", &CellList::getNeighboursForPosition);
+}
---
+++ dscribe/ext/soapGeneral.h
@@ -0,0 +1,28 @@
+#ifndef SOAPGENERAL_H
+#define SOAPGENERAL_H
+
+#include <vector>
+#include <pybind11/numpy.h>
+
+namespace py = pybind11;
+using namespace std;
+
+double* factorListSet();
+double* getws();
+double factorY(int l, int m, double* c);
+double* getoOr(double* r, int rsize);
+double* getrw2(double* r, int rsize);
+void expMs(double* rExpDiff, double alpha, double* r, double* ri, int isize, int rsize);
+void expPs(double* rExpSum, double alpha, double* r, double* ri, int isize, int rsize);
+void getDeltas(double* xNow, double* yNow, double* zNow, double* ri, double* rw, double rCut, double* oOri, double* oO4arri, double* minExp, double* pluExp,int* isCenter, double alpha, const py::array_t<double> &positions, const double ix, const double iy, const double iz, const vector<int> &indices, int rsize, int Ihpos, int Itype);
+double* getFlir(double* oO4arri,double* ri, double* minExp, double* pluExp, int icount, int rsize, int lMax);
+double legendre_poly(int l, int m, double x);
+double* getYlmi(double* x, double* y, double* z, double* oOri, double* cf, int icount, int lMax);
+double* getIntegrand(double* Flir, double* Ylmi,int rsize, int icount, int lMax);
+void getC(double* Cs, double* ws, double* rw2, double * gns, double* summed, double rCut,int lMax, int rsize, int gnsize,int* isCenter, double alpha);
+void accumC(double* Cts, double* Cs, int lMax, int gnsize, int typeI);
+void getPs(double* Ps, double* Cts,  int Nt, int lMax, int gnsize);
+void accumP(double* Phs, double* Ps, int Nt, int lMax, int gnsize, double rCut2, int Ihpos);
+void soapGeneral(py::array_t<double> cArr, py::array_t<double> AposArr, py::array_t<double> HposArr, py::array_t<int> atomicNumbersArr, double rCut, double cutoffPadding, int totalAN,int Nt,int gnsize, int lMax, int Hs, double alpha, py::array_t<double> rwArr, py::array_t<double> gssArr);
+
+#endif
