e715956bdbb81342635103a4c1b7ef2754bd20e4
Lauri Himanen <lauri.himanen@gmail.com>
2020-03-11 21:53:51 +0200

    Modified build stage.

---
+++ build.sh
@@ -0,0 +1,16 @@
+# Update pip
+pip install pip --upgrade
+
+# Install cython separately
+pip install cython
+
+# Install development dependencies
+pip install -r devrequirements.txt
+
+# Compile CMBTR and ACSF extensions with cython. The .so files will be compiled
+# during package setup
+cythonize dscribe/libmbtr/cmbtrwrapper.pyx
+cythonize dscribe/libacsf/acsfwrapper.pyx
+
+# Install the package itself
+pip install .
