bc49bd9b9cd89d31d6c7bc9b9d19d547491d576c
Lauri Himanen <lauri.himanen@gmail.com>
2020-03-12 09:43:06 +0200

    Re-cythonized wrapper files with newer version of Cython that supports python3.8.

--- .travis-install-dependencies.sh
+++ .travis-install-dependencies.sh
@@ -6,7 +6,9 @@ pip install -r devrequirements.txt
 
 # Compile CMBTR and ACSF extensions with cython. The .so files will be compiled
 # during package setup
-cythonize dscribe/libmbtr/cmbtrwrapper.pyx
-cythonize dscribe/libacsf/acsfwrapper.pyx
+cd dscribe/libmbtr/
+cythonize mbtrwrapper.pyx -X language_level=3
+cd ../libacsf/
+cythonize acsfwrapper.pyx -X language_level=3
 
 pip install .
