9616d15dd0c8fbd776fb8c14e6e63e9ebdc309dd
Azure Pipelines CI <lauri.himanen@gmail.com>
2020-07-23 13:36:44 +0300

    Fixed indexing bug in getPs for GTO.

--- dscribe/ext/soapGTO.cpp
+++ dscribe/ext/soapGTO.cpp
@@ -351,8 +351,8 @@ void getCfactors(double* preCoef, int Asize, double* x, double* y, double* z, do
   }
 }
 //================================================================
-void getC(double* C, double* preCoef, double* x, double* y, double* z,double* r2, double* bOa, double* aOa, double* exes,  int totalAN, int Asize, int Ns, int Ntypes, int lMax, int posI, int typeJ, int Nx2, int Nx3, int Nx4, int Nx5, int Nx6, int Nx7, int Nx8, int Nx9, int Nx10, int Nx11, int Nx12, int Nx13, int Nx14, int Nx15, int Nx16, int Nx17, int Nx18, int Nx19, int Nx20, int Nx21, int Nx22, int Nx23, int Nx24, int Nx25, int Nx26, int Nx27, int Nx28, int Nx29, int Nx30, int Nx31, int Nx32, int Nx33, int Nx34, int Nx35, int Nx36, int Nx37, int Nx38, int Nx39, int Nx40, int Nx41, int Nx42, int Nx43, int Nx44, int Nx45, int Nx46, int Nx47, int Nx48, int Nx49, int Nx50, int Nx51, int Nx52, int Nx53, int Nx54, int Nx55, int Nx56, int Nx57, int Nx58, int Nx59, int Nx60, int Nx61, int Nx62, int Nx63, int Nx64, int Nx65, int Nx66, int Nx67, int Nx68, int Nx69, int Nx70, int Nx71, int Nx72, int Nx73, int Nx74, int Nx75, int Nx76, int Nx77, int Nx78, int Nx79, int Nx80, int Nx81, int Nx82, int Nx83, int Nx84, int Nx85, int Nx86, int Nx87, int Nx88, int Nx89, int Nx90, int Nx91, int Nx92, int Nx93, int Nx94, int Nx95, int Nx96, int Nx97, int Nx98, int Nx99, int t2, int t3, int t4, int t5, int t6, int t7, int t8, int t9, int t10, int t11, int t12, int t13, int t14, int t15, int t16, int t17, int t18, int t19, int t20, int t21, int t22, int t23, int t24, int t25, int t26, int t27, int t28, int t29, int t30, int t31, int t32, int t33, int t34, int t35, int t36, int t37, int t38, int t39, int t40, int t41, int t42, int t43, int t44, int t45, int t46, int t47, int t48, int t49, int t50, int t51, int t52, int t53, int t54, int t55, int t56, int t57, int t58, int t59, int t60, int t61, int t62, int t63, int t64, int t65, int t66, int t67, int t68, int t69, int t70, int t71, int t72, int t73, int t74, int t75, int t76, int t77, int t78, int t79, int t80, int t81, int t82, int t83, int t84, int t85, int t86, int t87, int t88, int t89, int t90, int t91, int t92, int t93, int t94, int t95, int t96, int t97, int t98, int t99){
-
+void getC(double* C, double* preCoef, double* x, double* y, double* z,double* r2, double* bOa, double* aOa, double* exes,  int totalAN, int Asize, int Ns, int Ntypes, int lMax, int posI, int typeJ, int Nx2, int Nx3, int Nx4, int Nx5, int Nx6, int Nx7, int Nx8, int Nx9, int Nx10, int Nx11, int Nx12, int Nx13, int Nx14, int Nx15, int Nx16, int Nx17, int Nx18, int Nx19, int Nx20, int Nx21, int Nx22, int Nx23, int Nx24, int Nx25, int Nx26, int Nx27, int Nx28, int Nx29, int Nx30, int Nx31, int Nx32, int Nx33, int Nx34, int Nx35, int Nx36, int Nx37, int Nx38, int Nx39, int Nx40, int Nx41, int Nx42, int Nx43, int Nx44, int Nx45, int Nx46, int Nx47, int Nx48, int Nx49, int Nx50, int Nx51, int Nx52, int Nx53, int Nx54, int Nx55, int Nx56, int Nx57, int Nx58, int Nx59, int Nx60, int Nx61, int Nx62, int Nx63, int Nx64, int Nx65, int Nx66, int Nx67, int Nx68, int Nx69, int Nx70, int Nx71, int Nx72, int Nx73, int Nx74, int Nx75, int Nx76, int Nx77, int Nx78, int Nx79, int Nx80, int Nx81, int Nx82, int Nx83, int Nx84, int Nx85, int Nx86, int Nx87, int Nx88, int Nx89, int Nx90, int Nx91, int Nx92, int Nx93, int Nx94, int Nx95, int Nx96, int Nx97, int Nx98, int Nx99, int t2, int t3, int t4, int t5, int t6, int t7, int t8, int t9, int t10, int t11, int t12, int t13, int t14, int t15, int t16, int t17, int t18, int t19, int t20, int t21, int t22, int t23, int t24, int t25, int t26, int t27, int t28, int t29, int t30, int t31, int t32, int t33, int t34, int t35, int t36, int t37, int t38, int t39, int t40, int t41, int t42, int t43, int t44, int t45, int t46, int t47, int t48, int t49, int t50, int t51, int t52, int t53, int t54, int t55, int t56, int t57, int t58, int t59, int t60, int t61, int t62, int t63, int t64, int t65, int t66, int t67, int t68, int t69, int t70, int t71, int t72, int t73, int t74, int t75, int t76, int t77, int t78, int t79, int t80, int t81, int t82, int t83, int t84, int t85, int t86, int t87, int t88, int t89, int t90, int t91, int t92, int t93, int t94, int t95, int t96, int t97, int t98, int t99)
+{
   if(Asize == 0){return;}
   double sumMe = 0; int NsNs = Ns*Ns;  int NsJ = 100*Ns*typeJ; int LNsNs;
   int LNs; int NsTsI = 100*Ns*Ntypes*posI;
@@ -645,11 +645,12 @@ void getP(py::detail::unchecked_mutable_reference<double, 2> &cArr, double* Cnnd
     };
 
     // The current index in the final power spectrum array.
-    int pIdx = 0;
+    int pIdx;
     int NsTs100 = nMax*Nt*100;
     int Ns100 = nMax*100;
 
     for (int i = 0; i < Hs; i++) {
+        pIdx = 0;
         for (int Z1 = 0; Z1 < Nt; Z1++) {
             int Z2Limit = crossover ? Nt : Z1+1;
             for (int Z2 = Z1; Z2 < Z2Limit; Z2++) {
@@ -847,47 +848,47 @@ void soapGTO(py::array_t<double> cArr, py::array_t<double> positions, py::array_
         }
     }
 
-    //free(dx);
-    //free(dy);
-    //free(dz);
-    //free(z2);
-    //free(z4);
-    //free(z6);
-    //free(z8);
-    //free(r2);
-    //free(r4);
-    //free(r6);
-    //free(r8);
-    //free(ReIm2);
-    //free(ReIm3);
-    //free(ReIm4);
-    //free(ReIm5);
-    //free(ReIm6);
-    //free(ReIm7);
-    //free(ReIm8);
-    //free(ReIm9);
-    //free(exes);
-    //free(preCoef);
-    //free(bOa);
-    //free(aOa);
+    free(dx);
+    free(dy);
+    free(dz);
+    free(z2);
+    free(z4);
+    free(z6);
+    free(z8);
+    free(r2);
+    free(r4);
+    free(r6);
+    free(r8);
+    free(ReIm2);
+    free(ReIm3);
+    free(ReIm4);
+    free(ReIm5);
+    free(ReIm6);
+    free(ReIm7);
+    free(ReIm8);
+    free(ReIm9);
+    free(exes);
+    free(preCoef);
+    free(bOa);
+    free(aOa);
 
-    if (average == "inner") {
-        // average over axis 0 in cnnd matrix
-        for (int k = 0; k < Hs; k++) {
-            for (int k1 = 0; k1 < 100*Nt*Ns; k1++) {
-                cnndAve[k1] += cnnd[k1 + 100*Nt*Ns * k];
-            }
-        }
-        for (int k1 = 0; k1 < 100*Nt*Ns; k1++) {
-            cnndAve[k1] = cnndAve[k1] / (double)Hs;
-        }
-        getP(c, cnndAve, Ns, Nt, 1, lMax, crossover);
-    } else {
-        getP(c, cnnd, Ns, Nt, Hs, lMax, crossover);
-    }
+    //if (average == "inner") {
+        //// average over axis 0 in cnnd matrix
+        //for (int k = 0; k < Hs; k++) {
+            //for (int k1 = 0; k1 < 100*Nt*Ns; k1++) {
+                //cnndAve[k1] += cnnd[k1 + 100*Nt*Ns * k];
+            //}
+        //}
+        //for (int k1 = 0; k1 < 100*Nt*Ns; k1++) {
+            //cnndAve[k1] = cnndAve[k1] / (double)Hs;
+        //}
+        //getP(c, cnndAve, Ns, Nt, 1, lMax, crossover);
+    //} else {
+    getP(c, cnnd, Ns, Nt, Hs, lMax, crossover);
+    //}
 
-    //free(cnnd);
-    //free(cnndAve);
+    free(cnnd);
+    free(cnndAve);
 
     return;
 }
