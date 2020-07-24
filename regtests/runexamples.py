61475bdad28a16da1d749e09577523f9a8a73c0f
Azure Pipelines CI <lauri.himanen@gmail.com>
2020-07-24 14:49:42 +0300

    Simplified example test runner, fixed some old examples.

--- regtests/runexamples.py
+++ regtests/runexamples.py
@@ -1,35 +1,25 @@
 import sys, os
-#import math
-#import numpy as np
 import unittest
-
-#from dscribe.core import System
-#from dscribe.descriptors import ACSF
-#from dscribe.utils.species import symbols_to_numbers
-#from dscribe.ext import CellList
-
-#from ase.lattice.cubic import SimpleCubicFactory
-#from ase.build import bulk
-#import ase.data
-#from ase import Atoms
-#from dscribe.descriptors import SOAP
 import examples
-
-class cd:
-    """Context manager for changing the current working directory"""
-    def __init__(self, newPath):
-        self.newPath = os.path.expanduser(newPath)
-
-    def __enter__(self):
-        self.savedPath = os.getcwd()
-        os.chdir(self.newPath)
-
-    def __exit__(self, etype, value, traceback):
-        os.chdir(self.savedPath)
-
 
 
 class ExampleTests(unittest.TestCase):
+
+    savedPath = None
+
+    @classmethod
+    def setUpClass(cls):
+        """Navigates to the examples folder before running the examples.
+        """
+        cls.savedPath = os.getcwd()
+        os.chdir("examples")
+
+    @classmethod
+    def tearDownClass(cls):
+        """Navigates back the the previous directory after running the
+        examples.
+        """
+        os.chdir(cls.savedPath)
 
     def test_example_acsf(self):
         """Tests if the acsf.py example script runs without error.
@@ -69,29 +59,23 @@ class ExampleTests(unittest.TestCase):
     def test_example_aseatoms(self):
         """Tests if the aseatoms.py example script runs without error.
         """
-        with cd("examples"):
-            import examples.aseatoms
-
+        import examples.aseatoms
 
     def test_example_basics(self):
         """Tests if the basics.py example script runs without error.
         """
-        with cd("examples"):
-            import examples.basics
+        import examples.basics
 
     def test_example_readme(self):
         """Tests if the readme.py example script runs without error.
         """
-        with cd("examples"):
-            import examples.readme
-
-
+        import examples.readme
 
 if __name__ == '__main__':
     suites = []
     suites.append(unittest.TestLoader().loadTestsFromTestCase(ExampleTests))
     alltests = unittest.TestSuite(suites)
-    result = unittest.TextTestRunner(verbosity=0).run(alltests)
+    result = unittest.TextTestRunner(verbosity=0, buffer=True).run(alltests)
 
     # We need to return a non-zero exit code for the gitlab CI to detect errors
     sys.exit(not result.wasSuccessful())
