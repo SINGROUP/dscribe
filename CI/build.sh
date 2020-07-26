0825d749c3c2e9751b30e2aa94f025f4a1b643b3
Azure Pipelines CI <lauri.himanen@gmail.com>
2020-07-27 00:27:19 +0300

    Got rid of Cython build procedure.

--- CI/build.sh
+++ CI/build.sh
@@ -2,19 +2,9 @@
 pip install pip --upgrade
 pip install setuptools --upgrade
 
-# Install cython separately
-pip install cython
-
 # Install development dependencies
 cd ..
 pip install -r devrequirements.txt
-
-# Compile CMBTR and ACSF extensions with cython. The .so files will be compiled
-# during package setup
-cd dscribe/libmbtr/
-cythonize mbtrwrapper.pyx -X language_level=3
-cd ../libacsf/
-cythonize acsfwrapper.pyx -X language_level=3
 
 # Make a source distribution
 cd ../..
