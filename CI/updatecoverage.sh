90aa6c4b1397a223f1330e8bb93bbb24726e8078
Azure Pipelines CI <lauri.himanen@gmail.com>
2020-07-31 13:21:11 +0300

    Fixing coverage upload.

--- CI/updatecoverage.sh
+++ CI/updatecoverage.sh
@@ -1,4 +1,5 @@
 # Generate coverage report. The environment variable COVERALLS_REPO_TOKEN needs
 # to be defined for sending the report to coveralls.
 cd ..
+git checkout master
 coveralls --service=coveralls-python
