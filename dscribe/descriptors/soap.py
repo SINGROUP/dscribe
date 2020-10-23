Fixed issue in checking averaging mode.

dscribe/descriptors/soap.py
@@ -163,8 +163,10 @@ class SOAP(Descriptor):
         self._nmax = nmax
         self._lmax = lmax
         self._rbf = rbf
-        self._average = average
+        self.average = average
         self.crossover = crossover
+
+
 
     def prepare(self, system, cutoff_padding, positions=None):
         """Prepares the input for the C++ extension.
@@ -264,7 +266,7 @@ class SOAP(Descriptor):
         """
         if self._sparse:
             raise ValueError("Sparse output is not available for derivatives.")
-        if self._average is not "none" and method == "analytical":
+        if self.average != "off" and method == "analytical":
             raise ValueError(
                 "Analytical derivatives not available for averaged output."
             )
@@ -315,7 +317,6 @@ class SOAP(Descriptor):
         # Calculate numerically with extension
         if method == "numerical":
             if self._rbf == "gto":
-                print(sorted_pos)
                 dscribe.ext.derivatives_soap_gto(
                     d,
                     c,
@@ -335,7 +336,7 @@ class SOAP(Descriptor):
                     n_centers,
                     self._eta,
                     self.crossover,
-                    self._average,
+                    self.average,
                     return_descriptor,
                 )
         elif method == "analytical":
@@ -351,14 +352,14 @@ class SOAP(Descriptor):
         return d
 
     def init_descriptor_array(self, n_centers, n_features):
-        if self._average == "inner" or self._average == "outer":
+        if self.average == "inner" or self.average == "outer":
             c = np.zeros((1, n_features), dtype=np.float64)
         else:
             c = np.zeros((n_centers, n_features), dtype=np.float64)
         return c
 
     def init_derivatives_array(self, n_centers, n_atoms, n_features):
-        if self._average == "inner" or self._average == "outer":
+        if self.average == "inner" or self.average == "outer":
             d = np.zeros((1, n_atoms, 3, n_features), dtype=np.float64)
         else:
             d = np.zeros((n_centers, n_atoms, 3, n_features), dtype=np.float64)
@@ -417,7 +418,7 @@ class SOAP(Descriptor):
         output_sizes = []
         for i_job in jobs:
             n_desc = 0
-            if self._average == "outer" or self._average == "inner":
+            if self.average == "outer" or self.average == "inner":
                 n_desc = len(i_job)
             elif positions is None:
                 n_desc = 0
@@ -470,7 +471,7 @@ class SOAP(Descriptor):
                 lmax=self._lmax,
                 eta=self._eta,
                 crossover=self.crossover,
-                average=self._average,
+                average=self.average,
                 atomic_numbers=None,
             )
         elif self._rbf == "polynomial":
@@ -483,7 +484,7 @@ class SOAP(Descriptor):
                 lmax=self._lmax,
                 eta=self._eta,
                 crossover=self.crossover,
-                average=self._average,
+                average=self.average,
                 atomic_numbers=None,
             )
 

regtests/derivatives.py
@@ -31,63 +31,67 @@ from ase.build import molecule
 from ase.visualize import view
 
 H2 = Atoms(
-    positions=[
-        [-0.5, 0.5, 0.5],
-        [0.5, 0.5, 0.5],
-        # [1, 0.5, 1],
+    cell=[
+        [15.0, 0.0, 0.0],
+        [0.0, 15.0, 0.0],
+        [0.0, 0.0, 15.0]
     ],
-    symbols=["O", "H"],
+    positions=[
+        [-0.5, 0, 0],
+        [0.5, 0, 0],
+
+    ],
+    symbols=["H", "H"],
 )
-# H = Atoms(
-    # positions=[[0.5, 0.5, 0.5]],
-    # symbols=["H"],
-# )
-H = H2
+H = Atoms(
+    positions=[[0.5, 0.5, 0.5]],
+    symbols=["H"],
+)
 H2O = molecule("H2O")
 
 
 class SoapDerivativeTests(unittest.TestCase):
 
-    # def test_interface(self):
-        # """Test the derivative interface.
-        # """
-        # soap = SOAP(
-            # species=[1, 8],
-            # rcut=3,
-            # nmax=2,
-            # lmax=0,
-            # sparse=False,
-        # )
-        # positions = [[0.0, 0.0, 0.0]]
+    def test_interface(self):
+        """Test the derivative interface.
+        """
+        soap = SOAP(
+            species=[1, 8],
+            rcut=3,
+            nmax=2,
+            lmax=0,
+            sparse=False,
+        )
+        positions = [[0.0, 0.0, 0.0]]
 
-        # # Test that trying to do sparse output raises an exception
-        # soap.sparse = True
-        # with self.assertRaises(ValueError):
-            # soap.derivatives_single(H2, positions=positions, method="numerical")
+        # Test that trying to do sparse output raises an exception
+        soap.sparse = True
+        with self.assertRaises(ValueError):
+            soap.derivatives_single(H2, positions=positions, method="numerical")
 
-        # # Test that trying to get analytical derivatives with averaged output
-        # # raises an exception
-        # soap.sparse = False
-        # soap.average = "inner"
-        # with self.assertRaises(ValueError):
-            # soap.derivatives_single(H2, positions=positions, method="analytical")
-        # soap.average = "none"
+        # Test that trying to get analytical derivatives with averaged output
+        # raises an exception
+        soap.sparse = False
+        soap.average = "inner"
+        with self.assertRaises(ValueError):
+            soap.derivatives_single(H2, positions=positions, method="analytical")
+        soap.average = "off"
 
-        # # Test include
-        # with self.assertRaises(ValueError):
-            # soap.derivatives_single(H2O, positions=positions, include=[])
-        # with self.assertRaises(ValueError):
-            # soap.derivatives_single(H2O, positions=positions, include=[3])
-        # s = soap.derivatives_single(H2O, positions=positions, include=[2, 0, 0])
-        # self.assertEqual(s.shape[1], 2)
+        # Test include
+        with self.assertRaises(ValueError):
+            soap.derivatives_single(H2O, positions=positions, include=[])
+        with self.assertRaises(ValueError):
+            soap.derivatives_single(H2O, positions=positions, include=[3])
+        s = soap.derivatives_single(H2O, positions=positions, include=[2, 0, 0], return_descriptor=False)
+        self.assertEqual(s.shape[1], 2)
 
-        # # Test exclude
-        # s = soap.derivatives_single(H2O, positions=positions, exclude=[])
-        # self.assertEqual(s.shape[1], 3)
-        # with self.assertRaises(ValueError):
-            # soap.derivatives_single(H2O, positions=positions, exclude=[3])
-        # s = soap.derivatives_single(H2O, positions=positions, exclude=[0, 2, 2])
-        # self.assertEqual(s.shape[1], 1)
+        # Test exclude
+        s = soap.derivatives_single(H2O, positions=positions, exclude=[], return_descriptor=False)
+        self.assertEqual(s.shape[1], 3)
+        with self.assertRaises(ValueError):
+            soap.derivatives_single(H2O, positions=positions, exclude=[3])
+        s = soap.derivatives_single(H2O, positions=positions, exclude=[0, 2, 2], return_descriptor=False)
+        self.assertEqual(s.shape[1], 1)
 
     def test_numerical(self):
         """Test numerical values.
@@ -102,14 +106,13 @@ class SoapDerivativeTests(unittest.TestCase):
         )
 
         # Calculate with forward finite difference
-        positions = [[0.0, 0.0, 0.0]]
-        # positions = [[0.0, 0.0, 0.0], [0.12, -0.5, 0.3]]
+        # positions = [[0.0, 0.0, 0.0]]
+        positions = [[0.0, 0.0, 0.0], [0.12, -0.5, 0.3]]
         h = 0.0000001
         n_atoms = len(H)
         n_pos = len(positions)
         n_features = soap.get_number_of_features()
         n_comp = 3
-        view(H)
         derivatives_python = np.zeros((n_pos, n_atoms, n_comp, n_features))
         for i_pos in range(len(positions)):
             center = positions[i_pos]
@@ -117,8 +120,8 @@ class SoapDerivativeTests(unittest.TestCase):
             for i_atom in range(len(H)):
                 for i_comp in range(3):
                     H_disturbed = H.copy()
-                    # translation = np.array([[0, 0, 0]], dtype=np.float64)
-                    translation = np.array([[0, 0, 0], [0, 0, 0]], dtype=np.float64)
+                    translation = np.array([[0, 0, 0]], dtype=np.float64)
+                    # translation = np.array([[0, 0, 0], [0, 0, 0]], dtype=np.float64)
                     # translation = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]], dtype=np.float64)
                     translation[i_atom, i_comp] = h
                     H_disturbed.translate(translation)
@@ -128,13 +131,13 @@ class SoapDerivativeTests(unittest.TestCase):
 
         # Calculate with central finite difference implemented in C++
         derivatives_cpp, d1 = soap.derivatives_single(H, positions=positions, method="numerical")
+
+        # Test that desriptor values are correct
         d2 = soap.create(H, positions=positions)
         self.assertTrue(np.allclose(d1, d2, atol=1e-5))
-        # print(d1-d2)
 
         # Compare values
-        print(np.max(np.abs(derivatives_python-derivatives_cpp)))
-        # self.assertTrue(np.allclose(derivatives_python, derivatives_cpp, atol=1e-5))
+        self.assertTrue(np.allclose(derivatives_python, derivatives_cpp, atol=1e-5))
 
     # def test_analytical(self):
         # """Tests if the analytical soap derivatives run