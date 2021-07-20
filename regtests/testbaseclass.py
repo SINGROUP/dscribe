Fix weighting on k3 term
Fix index error on k3 weighting. ValleOganov subclass now passes all
of its current tests.

regtests/testbaseclass.py
@@ -92,13 +92,13 @@ class TestBaseClass(ABC):
         features = create(finite_system)
         is_perm_sym = True
 
-        for permutation in ([0, 2, 1], [1, 0, 2], [1, 0, 2], [2, 1, 0], [2, 0, 1]):
+        for permutation in ([0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 1, 0], [2, 0, 1]):
             i_system = finite_system[permutation]
             i_features = create(i_system)
             deviation = np.max(np.abs(features - i_features))
             if deviation > 1e-7:
                 is_perm_sym = False
-
+                
         return is_perm_sym
 
     def dict_comparison(self, first, second):