a5b003d1f043180c79fcd1079345abfe4d962bed
Lauri Himanen <lauri.himanen@gmail.com>
2021-08-18 22:03:30 +0300

    Updated version number.

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ extensions = [
 if __name__ == "__main__":
     setup(
         name="dscribe",
-        version="1.1.0",
+        version="1.2.0",
         url="https://singroup.github.io/dscribe/",
         description="A Python package for creating feature transformations in applications of machine learning to materials science.",
         long_description="A Python package for creating feature transformations in applications of machine learning to materials science.",
