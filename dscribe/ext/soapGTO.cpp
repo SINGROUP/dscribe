7f8bc35d52b88ce993cb1dd34b35aa5b675b0038
Lauri Himanen <6513318+lauri-codes@users.noreply.github.com>
2021-10-13 21:56:02 +0300

    Memory leak fix (#71)
    Fixed memory leak in SOAP

--- dscribe/ext/soapGTO.cpp
+++ dscribe/ext/soapGTO.cpp
@@ -2199,7 +2199,9 @@ shift = 0;
       }
     }
   }
-}}
+}
+free(preExponentArrya);
+}
 //================================================================================================
 /**
  * Used to calculate the partial power spectrum.
@@ -2477,12 +2479,17 @@ void soapGTO(
   free(dx); free(x2); free(x4); free(x6); free(x8); free(x10); free(x12); free(x14); free(x16); free(x18);
   free(dy); free(y2); free(y4); free(y6); free(y8); free(y10); free(y12); free(y14); free(y16); free(y18);
   free(dz); free(z2); free(z4); free(z6); free(z8); free(z10); free(z12); free(z14); free(z16); free(z18);
+  free(r1);
   free(r2); free(r4); free(r6); free(r8); free(r10); free(r12); free(r14); free(r16); free(r18);
   free(r20);
   free(x20);
   free(y20);
   free(z20);
-  free(exes); free(preCoef); free(bOa); free(aOa); free(cnnd_raw);
+  free(exes); free(preCoef); free(bOa); free(aOa); free(cnnd_raw); free(weights);
+
+  if (return_derivatives) {
+  free(prCofDX); free(prCofDY); free(prCofDZ);
+  }
 
   // Calculate the descriptor value if requested
   if (return_descriptor) {
