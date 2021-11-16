7f81e0fa6a93e1b8408c01bdfb58e2a3f1b652a9
Lauri Himanen <lauri.himanen@gmail.com>
2021-11-16 22:21:13 +0200

    Added ValleOganov tests to be run with the CI pipeline.

--- regtests/testrunner.py
+++ regtests/testrunner.py
@@ -9,6 +9,7 @@ import matrixpermutation
 import acsf
 import mbtr
 import lmbtr
+import valleoganov
 import soap
 import elementaldistribution
 import kernels
@@ -27,6 +28,7 @@ suite.addTests(loader.loadTestsFromModule(sinematrix))
 suite.addTests(loader.loadTestsFromModule(acsf))
 suite.addTests(loader.loadTestsFromModule(mbtr))
 suite.addTests(loader.loadTestsFromModule(lmbtr))
+suite.addTests(loader.loadTestsFromModule(valleoganov))
 suite.addTests(loader.loadTestsFromModule(soap))
 suite.addTests(loader.loadTestsFromModule(elementaldistribution))
 suite.addTests(loader.loadTestsFromModule(kernels))
