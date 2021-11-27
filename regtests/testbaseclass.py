cf34580db020b9c6a7a0260e132c9c9c74d82835
Lauri Himanen <lauri.himanen@gmail.com>
2021-11-27 21:22:46 +0200

    Testing rotational symmetry threshold.

--- regtests/testbaseclass.py
+++ regtests/testbaseclass.py
@@ -59,7 +59,7 @@ class TestBaseClass(ABC):
             i_system.rotate(45, rotation, rotate_cell=True)
             i_features = create(i_system)
             deviation = np.max(np.abs(features - i_features))
-            if deviation > 1e-4:
+            if deviation > 2e-4:
                 is_rot_sym = False
         return is_rot_sym
 
