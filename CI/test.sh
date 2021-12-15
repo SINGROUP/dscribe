ffd4feeb9f2d03dac32937be9dc00eb34692e362
Lauri Himanen <lauri.himanen@gmail.com>
2021-12-15 22:59:30 +0200

    Trying to catch test errors correctly.

--- CI/test.sh
+++ CI/test.sh
@@ -2,4 +2,7 @@
 cd ../regtests
 export COVERAGE_FILE="../.coverage"
 coverage run --source="dscribe" testrunner.py
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 coverage run -m --source="dscribe" --append pytest
