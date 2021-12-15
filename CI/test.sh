bd18f59c78d68e9024bf9d74711491145ded6bb1
Lauri Himanen <lauri.himanen@gmail.com>
2021-12-15 23:13:07 +0200

    Fixed test script.

--- CI/test.sh
+++ CI/test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 cd ../regtests
 export COVERAGE_FILE="../.coverage"
-coverage run --source="dscribe" testrunner.py
-if [ $? -ne 0 ]; then
+unittest=`coverage run --source="dscribe" testrunner.py`
+pytest=`coverage run -m --source="dscribe" --append pytest`
+if [ $unittest != 0 ] || [ $pytest != 0 ]; then
     exit 1
 fi
-coverage run -m --source="dscribe" --append pytest
