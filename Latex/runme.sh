72aff556259ef42c4da4c34e7267af8638134369
Aki78 <kmorooka78@gmail.com>
2023-01-03 16:43:14 +0200

    added smal instructions

--- Latex/runme.sh
+++ Latex/runme.sh
@@ -1,3 +1,5 @@
+#copy each matrix from the maxima results (including drivative,x ,y and z)
+#which have to be generated each individually. currently the size is set to 21.
 #if the size of matrix is different, the size must be changed in the .lua file
 lua ./convert.lua Raw/phi_raw.tex > Results/phi.tex
 lua ./convert.lua Raw/phi_rawDX.tex > Results/phiDX.tex
