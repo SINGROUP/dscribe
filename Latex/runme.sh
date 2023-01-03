dd3a2144a1487a36b946a62fcec0eaa704df2449
Aki78 <kmorooka78@gmail.com>
2023-01-03 16:28:45 +0200

    raw to Raw directory

--- Latex/runme.sh
+++ Latex/runme.sh
@@ -1,5 +1,5 @@
 #if the size of matrix is different, the size must be changed in the .lua file
-lua ./convert.lua phi_raw.tex > Results/phi.tex
-lua ./convert.lua phi_rawDX.tex > Results/phiDX.tex
-lua ./convert.lua phi_rawDY.tex > Results/phiDY.tex
-lua ./convert.lua phi_rawDZ.tex > Results/phiDZ.tex
+lua ./convert.lua Raw/phi_raw.tex > Results/phi.tex
+lua ./convert.lua Raw/phi_rawDX.tex > Results/phiDX.tex
+lua ./convert.lua Raw/phi_rawDY.tex > Results/phiDY.tex
+lua ./convert.lua Raw/phi_rawDZ.tex > Results/phiDZ.tex
