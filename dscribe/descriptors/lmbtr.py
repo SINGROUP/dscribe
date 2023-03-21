Fixed issue in lmbtr normalization.

dscribe/descriptors/lmbtr.py
@@ -18,6 +18,7 @@ import math
 
 import numpy as np
 import scipy.spatial.distance
+from sklearn.preprocessing import normalize
 import sparse
 from ase import Atoms
 import ase.data
@@ -412,15 +413,26 @@ class LMBTR(MBTR, DescriptorLocal):
         # Handle normalization
         if self.normalization == "l2_each":
             if self.flatten is True:
-                for key, value in mbtr.items():
+                for value in mbtr.values():
                     norm = np.linalg.norm(value.data)
                     value /= norm
             else:
-                for key, value in mbtr.items():
+                for value in mbtr.values():
                     for array in value:
                         i_data = array.ravel()
                         i_norm = np.linalg.norm(i_data)
                         array /= i_norm
+        elif self.normalization == "l2":
+            if self.flatten is True:
+                for value in mbtr.values():
+                    normalize(value.tocsr(), norm="l2", axis=1, copy=False)
+            else:
+                for value in mbtr.values():
+                    for array in value:
+                        norm += np.linalg.norm(array.ravel())
+                for value in mbtr.values():
+                    for array in value:
+                        array /= norm
 
         # Flatten output if requested
         if self.flatten:

dscribe/descriptors/mbtr.py
@@ -956,23 +956,20 @@ class MBTR(DescriptorGlobal):
         # Valle-Oganov normalization is calculated separately for each pair.
         # Not implemented for derivatives.
         if self.normalization == "valle_oganov":
-            S = self.system
-            V = S.cell.volume
-            imap = self.index_to_atomic_number
+            volume = self.system.cell.volume
             # Calculate the amount of each element for N_A*N_B term
-            counts = {}
-            for index, number in imap.items():
-                counts[index] = list(S.get_atomic_numbers()).count(number)
-            for i in range(n_elem):
-                for j in range(n_elem):
+            values, counts = np.unique(self.system.get_atomic_numbers(), return_counts=True)
+            counts = dict(zip(values, counts))
+            for i_z in values:
+                for j_z in values:
+                    i = self.atomic_number_to_index[i_z]
+                    j = self.atomic_number_to_index[j_z]
                     if j < i:
                         continue
                     if i == j:
-                        count_product = 0.5 * counts[i] * counts[j]
+                        count_product = 0.5 * counts[i_z] * counts[j_z]
                     else:
-                        count_product = counts[i] * counts[j]
-                    if count_product == 0:
-                        continue
+                        count_product = counts[i_z] * counts[j_z]
 
                     # This is the index of the spectrum. It is given by enumerating the
                     # elements of an upper triangular matrix from left to right and top
@@ -980,7 +977,7 @@ class MBTR(DescriptorGlobal):
                     m = int(j + i * n_elem - i * (i + 1) / 2)
                     start = m * n
                     end = (m + 1) * n
-                    y_normed = (k2[start:end] * V) / (count_product * 4 * np.pi)
+                    y_normed = (k2[start:end] * volume) / (count_product * 4 * np.pi)
 
                     k2[start:end] = y_normed
 
@@ -1120,16 +1117,16 @@ class MBTR(DescriptorGlobal):
         # Valle-Oganov normalization is calculated separately for each triplet
         # Not implemented for derivatives.
         if self.normalization == "valle_oganov":
-            S = self.system
-            V = S.cell.volume
-            imap = self.index_to_atomic_number
+            volume = self.system.cell.volume
             # Calculate the amount of each element for N_A*N_B*N_C term
-            counts = {}
-            for index, number in imap.items():
-                counts[index] = list(S.get_atomic_numbers()).count(number)
-            for i in range(n_elem):
-                for j in range(n_elem):
-                    for k in range(n_elem):
+            values, counts = np.unique(self.system.get_atomic_numbers(), return_counts=True)
+            counts = dict(zip(values, counts))
+            for i_z in values:
+                for j_z in values:
+                    for k_z in values:
+                        i = self.atomic_number_to_index[i_z]
+                        j = self.atomic_number_to_index[j_z]
+                        k = self.atomic_number_to_index[k_z]
                         if k < i:
                             continue
                         # This is the index of the spectrum. It is given by enumerating the
@@ -1144,10 +1141,8 @@ class MBTR(DescriptorGlobal):
                         )
                         start = m * n
                         end = (m + 1) * n
-                        count_product = counts[i] * counts[j] * counts[k]
-                        if count_product == 0:
-                            continue
-                        y_normed = (k3[start:end] * V) / count_product
+                        count_product = counts[i_z] * counts[j_z] * counts[k_z]
+                        y_normed = (k3[start:end] * volume) / count_product
                         k3[start:end] = y_normed
 
         # If non-flattened descriptor is requested, reshape the output
@@ -1249,11 +1244,7 @@ class MBTR(DescriptorGlobal):
             mbtr_d["k3"] = k3_d
 
         # Handle normalization
-        if self.normalization == "l2_each":
-            # Normalization factor is a function of atomic positions.
-            # Not implemented
-            pass
-        elif self.normalization == "n_atoms":
+        if self.normalization == "n_atoms":
             n_atoms = len(self.system)
             if self.flatten is True:
                 for key, value in mbtr.items():

regtests/conftest.py
@@ -389,9 +389,6 @@ def assert_parallellization(descriptor_func, n_jobs, flatten, sparse, positions=
 
     a = desc.create(samples[0], **a_kwargs)
     b = desc.create(samples[1], **b_kwargs)
-    # from ase.visualize import view
-    # print(a_kwargs)
-    # print(b_kwargs)
 
     # The output may be a list or an array.
     if isinstance(output, list):
@@ -420,7 +417,7 @@ def assert_no_system_modification(descriptor_func):
         pbc = np.array(system.get_pbc())
         atomic_numbers = np.array(system.get_atomic_numbers())
         symbols = np.array(system.get_chemical_symbols())
-        descriptor = descriptor_func()([system])
+        descriptor_func()([system])
         assert np.array_equal(cell, system.get_cell())
         assert np.array_equal(pos, system.get_positions())
         assert np.array_equal(pbc, system.get_pbc())
@@ -485,12 +482,11 @@ def assert_basis(descriptor_func):
     assert abs(dot - 1) < 1e-3
 
 
-def assert_normalization(descriptor_func, normalization):
+def assert_normalization(descriptor_func, system, normalization, norm_rel=None, norm_abs=None):
     """Tests that the normalization of the output is done correctly."""
-    system = water()
-    desc_raw = descriptor_func(normalization="none")([system])
+    desc_raw = descriptor_func(normalization="none", periodic=True)([system])
     features_raw = desc_raw.create(system)
-    desc_normalized = descriptor_func(normalization=normalization)([system])
+    desc_normalized = descriptor_func(normalization=normalization, periodic=True)([system])
     features_normalized = desc_normalized.create(system)
     is_local = isinstance(desc_normalized, DescriptorLocal)
     if is_local:
@@ -498,13 +494,14 @@ def assert_normalization(descriptor_func, normalization):
         features_normalized = features_normalized[0]
 
     norm = np.linalg.norm(features_normalized)
-    norm_raw = np.linalg.norm(features_raw)
-    if normalization == "l2":
-        norm == pytest.approx(1, 0, 1e-8)
-    elif normalization == "n_atoms":
-        norm == pytest.approx(norm_raw / 3, 0, 1e-8)
-    else:
-        raise ValueError("Unsupported normalization")
+    if norm_rel is not None and norm_abs is not None:
+        raise ValueError("Provide only relative or absolute norm")
+    if norm_rel is not None:
+        norm_raw = np.linalg.norm(features_raw)
+        assert norm_raw != 0
+        assert norm / norm_raw == pytest.approx(norm_rel, 0, 1e-8)
+    if norm_abs is not None:
+        assert norm == pytest.approx(norm_abs, 0, 1e-8)
 
 
 def assert_positions(descriptor_func):
@@ -624,7 +621,6 @@ def assert_matrix_descriptor_random(descriptor_func):
 
     # Get the mean value to compare to
     sigma = 5
-    n_atoms_max = 2
     desc = descriptor_func(permutation="sorted_l2", flatten=False)([HHe])
     features = desc.create(HHe)
     means = np.linalg.norm(features, axis=1)

regtests/test_mbtr.py
@@ -237,10 +237,15 @@ def test_derivatives(method, periodic, normalization, k2, k3):
     assert_derivatives(mbtr_func, method, periodic, water())
 
 
-@pytest.mark.parametrize("normalization", ["l2", "n_atoms"])
-def test_normalization(normalization):
+@pytest.mark.parametrize("normalization, norm_rel, norm_abs", [
+    ("l2", None, 1),
+    ("n_atoms", 1 / 4, None),
+    ("valle_oganov", 46.656 / (4 * 8 * np.pi), None),
+])
+def test_normalization(normalization, norm_rel, norm_abs):
     """Tests that the normalization works correctly."""
-    assert_normalization(mbtr_default_k2, normalization)
+    system = bulk("Cu", "fcc", cubic=True, a=3.6)
+    assert_normalization(mbtr_default_k2, system, normalization, norm_rel, norm_abs)
 
 
 @pytest.mark.parametrize("k", [1, 2, 3])