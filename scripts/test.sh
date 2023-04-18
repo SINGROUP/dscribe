fda162828066c7a3e92bbb7f7f0be2d89a0fbfcc
Lauri Himanen <lauri.himanen@gmail.com>
2023-04-18 19:43:03 +0300

    Fully migrated to pytest.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 cd ../tests
 export COVERAGE_FILE="../.coverage"
-coverage run --source="dscribe" testrunner.py
-unittest=$?
-coverage run -m --source="dscribe" --append pytest
-pytest=$?
-if [ "$unittest" != 0 ] || [ "$pytest" != 0 ]; then
-    exit 1
-fi
+coverage run -m --source="dscribe" pytest
