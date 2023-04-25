Fixed issues in local descriptor derivatives, improved testing.

dscribe/descriptors/descriptorlocal.py
@@ -351,13 +351,11 @@ class DescriptorLocal(Descriptor):
         deltas = [-1.0, 1.0]
         if positions is None: 
             positions = range(len(system))
-        if attach:
-            centers = positions
-        else:
-            centers = system.get_positions()[positions]
+        if not attach and np.issubdtype(type(positions[0]), np.integer):
+            positions = system.get_positions()[positions]
 
         for index, i_atom in enumerate(indices):
-            for i_center, center in enumerate(centers):
+            for i_center, center in enumerate(positions):
                 for i_comp in range(3):
                     for i_stencil in range(2):
                         system_disturbed = system.copy()

dscribe/descriptors/lmbtr.py
@@ -24,13 +24,13 @@ from ase import Atoms
 import ase.data
 
 from dscribe.core import System
-from dscribe.descriptors import MBTR
+from dscribe.descriptors.mbtr import check_weighting, check_k2, check_k3
 from dscribe.descriptors.descriptorlocal import DescriptorLocal
 from dscribe.ext import MBTRWrapper
 import dscribe.utils.geometry
 
 
-class LMBTR(MBTR, DescriptorLocal):
+class LMBTR(DescriptorLocal):
     """Implementation of local -- per chosen atom -- kind of the Many-body
     tensor representation up to k=3.
 
@@ -164,16 +164,84 @@ class LMBTR(MBTR, DescriptorLocal):
                     * ``"float64"``: Double precision floating point numbers.
         """
         super().__init__(
-            k1=None,
-            k2=k2,
-            k3=k3,
             periodic=periodic,
-            species=species,
-            normalization=normalization,
-            normalize_gaussians=normalize_gaussians,
             sparse=sparse,
             dtype=dtype,
         )
+        self.system = None
+        self.k2 = k2
+        self.k3 = k3
+        self.species = species
+        self.normalization = normalization
+        self.normalize_gaussians = normalize_gaussians
+
+        if self.normalization == "valle_oganov" and not periodic:
+            raise ValueError(
+                "Valle-Oganov normalization does not support non-periodic systems."
+            )
+
+        # Initializing .create() level variables
+        self._interaction_limit = None
+
+        # Check that weighting function is specified for periodic systems
+        check_weighting(self.periodic, self.k2)
+        check_weighting(self.periodic, self.k3)
+
+    @property
+    def k2(self):
+        return self._k2
+
+    @k2.setter
+    def k2(self, value):
+        check_k2(value)
+        self._k2 = value
+
+    @property
+    def k3(self):
+        return self._k3
+
+    @k3.setter
+    def k3(self, value):
+        check_k3(value)
+        self._k3 = value
+
+    @property
+    def species(self):
+        return self._species
+
+    @species.setter
+    def species(self, value):
+        """Used to check the validity of given atomic numbers and to initialize
+        the C-memory layout for them.
+
+        Args:
+            value(iterable): Chemical species either as a list of atomic
+                numbers or list of chemical symbols.
+        """
+        # The species are stored as atomic numbers for internal use.
+        self._set_species(value)
+
+        # The atomic number 0 is reserved for ghost atoms in this
+        # implementation.
+        if 0 in self._atomic_number_set:
+            raise ValueError(
+                "The atomic number 0 is reserved for the ghost atoms in this "
+                "implementation."
+            )
+        self._atomic_number_set.add(0)
+        indices = np.searchsorted(self._atomic_numbers, 0)
+        self._atomic_numbers = np.insert(self._atomic_numbers, indices, 0)
+
+        # Setup mappings between atom indices and types together with some
+        # statistics
+        self.atomic_number_to_index = {}
+        self.index_to_atomic_number = {}
+        for i_atom, atomic_number in enumerate(self._atomic_numbers):
+            self.atomic_number_to_index[atomic_number] = i_atom
+            self.index_to_atomic_number[i_atom] = atomic_number
+        self.n_elements = len(self._atomic_numbers)
+        self.max_atomic_number = max(self._atomic_numbers)
+        self.min_atomic_number = min(self._atomic_numbers)
 
     @property
     def normalization(self):
@@ -476,7 +544,6 @@ class LMBTR(MBTR, DescriptorLocal):
         Returns:
             1D ndarray: flattened K2 values.
         """
-        print(new_system)
         grid = self.k2["grid"]
         start = grid["min"]
         stop = grid["max"]
@@ -851,41 +918,3 @@ class LMBTR(MBTR, DescriptorLocal):
             end = int(offset + (m + 1) * n3)
 
         return slice(start, end)
-
-    @property
-    def species(self):
-        return self._species
-
-    @species.setter
-    def species(self, value):
-        """Used to check the validity of given atomic numbers and to initialize
-        the C-memory layout for them.
-
-        Args:
-            value(iterable): Chemical species either as a list of atomic
-                numbers or list of chemical symbols.
-        """
-        # The species are stored as atomic numbers for internal use.
-        self._set_species(value)
-
-        # The atomic number 0 is reserved for ghost atoms in this
-        # implementation.
-        if 0 in self._atomic_number_set:
-            raise ValueError(
-                "The atomic number 0 is reserved for the ghost atoms in this "
-                "implementation."
-            )
-        self._atomic_number_set.add(0)
-        indices = np.searchsorted(self._atomic_numbers, 0)
-        self._atomic_numbers = np.insert(self._atomic_numbers, indices, 0)
-
-        # Setup mappings between atom indices and types together with some
-        # statistics
-        self.atomic_number_to_index = {}
-        self.index_to_atomic_number = {}
-        for i_atom, atomic_number in enumerate(self._atomic_numbers):
-            self.atomic_number_to_index[atomic_number] = i_atom
-            self.index_to_atomic_number[i_atom] = atomic_number
-        self.n_elements = len(self._atomic_numbers)
-        self.max_atomic_number = max(self._atomic_numbers)
-        self.min_atomic_number = min(self._atomic_numbers)

dscribe/descriptors/soap.py
@@ -22,8 +22,6 @@ from ase import Atoms
 import ase.geometry.cell
 import ase.data
 
-import sparse as sp
-
 from dscribe.descriptors.descriptorlocal import DescriptorLocal
 import dscribe.ext
 
@@ -65,10 +63,6 @@ class SOAP(DescriptorLocal):
         periodic=False,
         sparse=False,
         dtype="float64",
-        # For backwards compatibility with < v1.2.2
-        rcut=None,
-        nmax=None,
-        lmax=None,
     ):
         """
         Args:
@@ -169,22 +163,6 @@ class SOAP(DescriptorLocal):
                     * ``"float64"``: Double precision floating point numbers.
 
         """
-        var_dict = {}
-        for var_new in ["r_cut", "n_max", "l_max"]:
-            loc = locals()
-            var_old = "".join(var_new.split("_"))
-            if loc.get(var_old) is not None:
-                var_dict[var_new] = loc[var_old]
-                if loc.get(var_new) is not None:
-                    raise ValueError(
-                        "Please provide only either {} or {}.".format(var_new, var_old)
-                    )
-            else:
-                var_dict[var_new] = loc[var_new]
-        r_cut = var_dict["r_cut"]
-        n_max = var_dict["n_max"]
-        l_max = var_dict["l_max"]
-
         supported_dtype = set(("float32", "float64"))
         if dtype not in supported_dtype:
             raise ValueError(
@@ -298,7 +276,7 @@ class SOAP(DescriptorLocal):
         self.average = average
         self.crossover = crossover
 
-    def prepare_centers(self, system, cutoff_padding, positions=None):
+    def prepare_centers(self, system, positions=None):
         """Validates and prepares the centers for the C++ extension."""
         # Check that the system does not have elements that are not in the list
         # of atomic numbers
@@ -503,7 +481,7 @@ class SOAP(DescriptorLocal):
             get_number_of_features()-function.
         """
         cutoff_padding = self.get_cutoff_padding()
-        centers, _ = self.prepare_centers(system, cutoff_padding, positions)
+        centers, _ = self.prepare_centers(system, positions)
         n_centers = centers.shape[0]
         pos = system.get_positions()
         Z = system.get_atomic_numbers()
@@ -653,9 +631,7 @@ class SOAP(DescriptorLocal):
         cell = ase.geometry.cell.complete_cell(system.get_cell())
         pbc = np.asarray(system.get_pbc(), dtype=bool)
         cutoff_padding = self.get_cutoff_padding()
-        centers, center_indices = self.prepare_centers(
-            system, cutoff_padding, positions
-        )
+        centers, center_indices = self.prepare_centers(system, positions)
 
         if self._rbf == "gto":
             alphas = self._alphas.flatten()
@@ -755,7 +731,7 @@ class SOAP(DescriptorLocal):
         cell = ase.geometry.cell.complete_cell(system.get_cell())
         pbc = np.asarray(system.get_pbc(), dtype=bool)
         cutoff_padding = self.get_cutoff_padding()
-        centers, _ = self.prepare_centers(system, cutoff_padding, positions)
+        centers, _ = self.prepare_centers(system, positions)
         sorted_species = self._atomic_numbers
         n_species = len(sorted_species)
         n_centers = centers.shape[0]

tests/conftest.py
@@ -266,28 +266,23 @@ def assert_derivatives(descriptor_func, method, pbc, system=big_system(), attach
     # Compare descriptor values
     assert np.allclose(d0, d_cpp, atol=1e-6)
 
+    # Check that derivatives are not super small
+    assert np.max(np.abs(derivatives_cpp)) > 1e-6
+
     # Compare derivative values
     assert np.allclose(derivatives_python, derivatives_cpp, rtol=1e-4, atol=1e-4)
 
 
-def assert_derivatives_include(descriptor_func, method):
+def assert_derivatives_include(descriptor_func, method, attach=None):
     H2O = molecule("H2O")
     CO2 = molecule("CO2")
     H2O.set_cell([5, 5, 5])
     CO2.set_cell([5, 5, 5])
     descriptor = descriptor_func([H2O, CO2])
-
-    # Invalid include options
-    with pytest.raises(ValueError):
-        descriptor.derivatives(H2O, include=[], attach=True, method=method)
-    with pytest.raises(ValueError):
-        descriptor.derivatives(H2O, include=[3], attach=True, method=method)
-    with pytest.raises(ValueError):
-        descriptor.derivatives(H2O, include=[-1], attach=True, method=method)
-    with pytest.raises(ValueError):
-        descriptor.derivatives(H2O, include=[0], exclude=[0], attach=True, method=method)
+    kwargs = {"method": method}
 
     if isinstance(descriptor, DescriptorLocal):
+        kwargs["attach"] = attach
         slice_d1_a = np.index_exp[:, 0, :]
         slice_d2_a = np.index_exp[:, 2, :]
         slice_d1_b = np.index_exp[:, 1, :]
@@ -314,39 +309,45 @@ def assert_derivatives_include(descriptor_func, method):
         slice_d1_f = np.index_exp[1, 0, :]
         slice_d2_f = np.index_exp[1, 1, :]
 
+    # Invalid include options
+    with pytest.raises(ValueError):
+        descriptor.derivatives(H2O, include=[], **kwargs)
+    with pytest.raises(ValueError):
+        descriptor.derivatives(H2O, include=[3], **kwargs)
+    with pytest.raises(ValueError):
+        descriptor.derivatives(H2O, include=[-1], **kwargs)
+    with pytest.raises(ValueError):
+        descriptor.derivatives(H2O, include=[0], exclude=[0], **kwargs)
+
     # Test that correct atoms are included and in the correct order
-    D1, d1 = descriptor.derivatives(H2O, include=[2, 0], attach=True, method=method)
-    D2, d2 = descriptor.derivatives(H2O, attach=True, method=method)
+    D1, d1 = descriptor.derivatives(H2O, include=[2, 0], **kwargs)
+    D2, d2 = descriptor.derivatives(H2O, **kwargs)
     assert np.array_equal(D1[slice_d1_a], D2[slice_d2_a])
     assert np.array_equal(D1[slice_d1_b], D2[slice_d2_b])
 
     # Test that using multiple samples and single include works
-    D1, d1 = descriptor.derivatives([H2O, CO2], include=[1, 0], attach=True, method=method)
-    D2, d2 = descriptor.derivatives([H2O, CO2], attach=True, method=method)
+    D1, d1 = descriptor.derivatives([H2O, CO2], include=[1, 0], **kwargs)
+    D2, d2 = descriptor.derivatives([H2O, CO2], **kwargs)
     assert np.array_equal(D1[slice_d1_c], D2[slice_d2_c])
     assert np.array_equal(D1[slice_d1_d], D2[slice_d2_d])
 
     # Test that using multiple samples and multiple includes
-    D1, d1 = descriptor.derivatives([H2O, CO2], include=[[0], [1]], attach=True, method=method)
-    D2, d2 = descriptor.derivatives([H2O, CO2], attach=True, method=method)
+    D1, d1 = descriptor.derivatives([H2O, CO2], include=[[0], [1]], **kwargs)
+    D2, d2 = descriptor.derivatives([H2O, CO2], **kwargs)
     assert np.array_equal(D1[slice_d1_e], D2[slice_d2_e])
     assert np.array_equal(D1[slice_d1_f], D2[slice_d2_f])
 
 
-def assert_derivatives_exclude(descriptor_func, method):
+def assert_derivatives_exclude(descriptor_func, method, attach=None):
     H2O = molecule("H2O")
     CO2 = molecule("CO2")
     H2O.set_cell([5, 5, 5])
     CO2.set_cell([5, 5, 5])
     descriptor = descriptor_func([H2O, CO2])
-
-    # Invalid exclude options
-    with pytest.raises(ValueError):
-        descriptor.derivatives(H2O, exclude=[3], attach=True, method=method)
-    with pytest.raises(ValueError):
-        descriptor.derivatives(H2O, exclude=[-1], attach=True, method=method)
+    kwargs = {"method": method}
 
     if isinstance(descriptor, DescriptorLocal):
+        kwargs["attach"] = attach
         slice_d1_a = np.index_exp[:, 0, :]
         slice_d2_a = np.index_exp[:, 0, :]
         slice_d1_b = np.index_exp[:, 1, :]
@@ -365,15 +366,21 @@ def assert_derivatives_exclude(descriptor_func, method):
         slice_d1_d = np.index_exp[:, 1, :]
         slice_d2_d = np.index_exp[:, 2, :]
 
+    # Invalid exclude options
+    with pytest.raises(ValueError):
+        descriptor.derivatives(H2O, exclude=[3], **kwargs)
+    with pytest.raises(ValueError):
+        descriptor.derivatives(H2O, exclude=[-1], **kwargs)
+
     # Test that correct atoms are excluded and in the correct order
-    D1, d1 = descriptor.derivatives(H2O, exclude=[1], attach=True, method=method)
-    D2, d2 = descriptor.derivatives(H2O, attach=True, method=method)
+    D1, d1 = descriptor.derivatives(H2O, exclude=[1], **kwargs)
+    D2, d2 = descriptor.derivatives(H2O, **kwargs)
     assert np.array_equal(D1[slice_d1_a], D2[slice_d2_a])
     assert np.array_equal(D1[slice_d1_b], D2[slice_d2_b])
 
     # Test that using single list and multiple samples works
-    D1, d1 = descriptor.derivatives([H2O, CO2], exclude=[1], attach=True, method=method)
-    D2, d2 = descriptor.derivatives([H2O, CO2], attach=True, method=method)
+    D1, d1 = descriptor.derivatives([H2O, CO2], exclude=[1], **kwargs)
+    D2, d2 = descriptor.derivatives([H2O, CO2], **kwargs)
     assert np.array_equal(D1[slice_d1_c], D2[slice_d2_c])
     assert np.array_equal(D1[slice_d1_d], D2[slice_d2_d])
 
@@ -487,9 +494,13 @@ def assert_parallellization(descriptor_func, n_jobs, sparse, positions=None, **k
             a = desc.create(samples[0], **a_kwargs)
             b = desc.create(samples[1], **b_kwargs)
         elif func == "derivatives":
-            output, _ = desc.derivatives(samples, n_jobs=n_jobs, attach=True, **all_kwargs)
-            a, _ = desc.derivatives(samples[0], attach=True, **a_kwargs)
-            b, _ = desc.derivatives(samples[1], attach=True, **b_kwargs)
+            if isinstance(desc, DescriptorLocal):
+                all_kwargs["attach"] = True
+                a_kwargs["attach"] = True
+                b_kwargs["attach"] = True
+            output, _ = desc.derivatives(samples, n_jobs=n_jobs, **all_kwargs)
+            a, _ = desc.derivatives(samples[0], **a_kwargs)
+            b, _ = desc.derivatives(samples[1], **b_kwargs)
 
         # The output may be a list or an array.
         if isinstance(output, list):