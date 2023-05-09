MBTR smooth_cutoff fix and derivatives (#103)
* Fix smooth cutoff bug

* Add derivatives for smooth cutoff

* Add derivatives for valle_oganov normalization

* Add MBTR derivative tests

* Add derivative test for ValleOganov k2 term

Adjusted the cutoff radii in ValleOganov tests so that they do not
exactly match any interatomic distance in the test system. Having
atoms directly at the cutoff would cause problems with derivative
tests because the k2-term weight function is not differentiable at
the cutoff.

* Add test for smooth_cutoff

* Fix formatting

dscribe/descriptors/mbtr.py
@@ -881,9 +881,10 @@ class MBTR(DescriptorGlobal):
                     m = int(j + i * n_elem - i * (i + 1) / 2)
                     start = m * n
                     end = (m + 1) * n
-                    y_normed = (k2[start:end] * volume) / (count_product * 4 * np.pi)
+                    norm_factor = volume / (count_product * 4 * np.pi)
 
-                    k2[start:end] = y_normed
+                    k2[start:end] *= norm_factor
+                    k2_d[:, :, start:end] *= norm_factor
 
         # Convert to the final output precision.
         if self.dtype == "float32":
@@ -929,8 +930,13 @@ class MBTR(DescriptorGlobal):
                     sharpness = weighting["sharpness"]
                 except Exception:
                     sharpness = 2
-                r_cut = weighting["r_cut"]
-                parameters = {b"sharpness": sharpness, b"cutoff": r_cut}
+                parameters = {b"sharpness": sharpness, b"cutoff": weighting["r_cut"]}
+                # Evaluating smooth-cutoff weighting values requires distances
+                # between two neighbours of an atom, and the maximum distance
+                # between them is twice the cutoff radius. To include the
+                # neighbour-to-neighbour distances in the distance matrix, the
+                # neighbour list is generated with double radius.
+                r_cut = 2 * weighting["r_cut"]
         else:
             weighting_function = "unity"
 
@@ -1033,8 +1039,10 @@ class MBTR(DescriptorGlobal):
                         start = m * n
                         end = (m + 1) * n
                         count_product = counts[i_z] * counts[j_z] * counts[k_z]
-                        y_normed = (k3[start:end] * volume) / count_product
-                        k3[start:end] = y_normed
+                        norm_factor = volume / count_product
+
+                        k3[start:end] *= norm_factor
+                        k3_d[:, :, start:end] *= norm_factor
 
         # Convert to the final output precision.
         if self.dtype == "float32":
@@ -1058,7 +1066,7 @@ class MBTR(DescriptorGlobal):
 
         # Check if analytical derivatives can be used
         try:
-            supported_normalization = ["none", "n_atoms"]
+            supported_normalization = ["none", "n_atoms", "valle_oganov"]
             if self.normalization not in supported_normalization:
                 raise ValueError(
                     "Analytical derivatives not implemented for normalization option '{}'. Please choose from: {}".format(
@@ -1067,12 +1075,6 @@ class MBTR(DescriptorGlobal):
                 )
             # Derivatives are not currently implemented for all k3 options
             if self.k3 is not None:
-                if self.k3.get("weighting") is not None:
-                    if self.k3["weighting"]["function"] == "smooth_cutoff":
-                        raise ValueError(
-                            "Analytical derivatives not implemented for k3 weighting function 'smooth_cutoff'."
-                        )
-
                 # "angle" function is not differentiable
                 if self.k3["geometry"]["function"] == "angle":
                     raise ValueError(