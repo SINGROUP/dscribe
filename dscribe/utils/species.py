b2c1ccbf70da9b13b593561700d5a2ca42adde34
Sherif Abdulkader Tawfik Abbas <53269434+sheriftawfikabbas@users.noreply.github.com>
2023-05-17 15:28:46 +1000

    change-np-int-to-int (#107)
    * change-np-int-to-int
    
    * removal-of-np.int-from-more-files

--- dscribe/utils/species.py
+++ dscribe/utils/species.py
@@ -59,13 +59,13 @@ def get_atomic_numbers(species):
         raise ValueError("Please provide the species as an iterable, e.g. a list.")
 
     # Determine if the given species are atomic numbers or chemical symbols
-    if all(isinstance(x, (int, np.integer)) for x in species):
+    if all(isinstance(x, int) for x in species):
         if any([x < 0 for x in species]):
             raise ValueError(
                 "The given list of species contains negative integers. Please use only non-negative integers"
             )
         atomic_numbers = species
-    elif all(isinstance(x, (str, np.str_)) for x in species):
+    elif all(isinstance(x, str) for x in species):
         atomic_numbers = symbols_to_numbers(species)
     else:
         raise ValueError(
